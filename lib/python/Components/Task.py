openhdf/enigma2

commit 19ed9ee4c17df5b2823efd88c77f17aa30dccc22

Harddisk, Task: Smarter backgrounding and better feedback
It kinda shows what went wrong if format or fsck fails.
Still needs more polishing, but it's an improvement already.

lib/python/Components/Task.py
@@ -182,9 +182,16 @@ class Task(object):
 		if failed_preconditions:
 			callback(self, failed_preconditions)
 			return
-		self.prepare()
 		self.callback = callback
-		self._run()
+		try:
+			self.prepare()
+			if (self.cmd is None) and (self.cmdline is None):
+				self.finish()
+			else: 
+				self._run()
+		except Exception, ex:
+			self.postconditions = [FailedPostcondition(ex)]
+			self.finish()
 
 	def prepare(self):
 		pass
@@ -260,6 +267,7 @@ class LoggingTask(Task):
 		Task.__init__(self, job, name)
 		self.log = []
 	def processOutput(self, data):
+		print "[%s]" % self.name, data,
 		self.log.append(data)
 
 
@@ -319,7 +327,7 @@ class JobManager:
 			if problems[0].RECOVERABLE:
 				Notifications.AddNotificationWithCallback(self.errorCB, MessageBox, _("Error: %s\nRetry?") % (problems[0].getErrorMessage(task)))
 			else:
-				Notifications.AddNotification(MessageBox, _("Error") + (': %s') % (problems[0].getErrorMessage(task)), type = MessageBox.TYPE_ERROR )
+				Notifications.AddNotification(MessageBox, job.name + "\n" + _("Error") + (': %s') % (problems[0].getErrorMessage(task)), type = MessageBox.TYPE_ERROR )
 				self.errorCB(False)
 			return
 			#self.failed_jobs.append(self.active_job)
@@ -431,11 +439,27 @@ class AbortedPostcondition(Condition):
 class ReturncodePostcondition(Condition):
 	def check(self, task):
 		return task.returncode == 0
+	def getErrorMessage(self, task):
+		if hasattr(task, 'log') and task.log:
+			log = ''.join(task.log).strip()
+			log = log.split('\n')[-3:]
+			log = '\n'.join(log)
+			return log
+		else:
+			return _("Error code") + ": %s" % task.returncode
 
 class FailedPostcondition(Condition):
 	def __init__(self, exception):
 		self.exception = exception
 	def getErrorMessage(self, task):
+		if isinstance(self.exception, int):
+			if hasattr(task, 'log'):
+				log = ''.join(task.log).strip()
+				log = log.split('\n')[-4:]
+				log = '\n'.join(log)
+				return log
+			else:
+				return _("Error code") + " %s" % self.exception
 		return str(self.exception)
 	def check(self, task):
 		return (self.exception is None) or (self.exception == 0) 

lib/python/Components/Harddisk.py
@@ -250,22 +250,18 @@ class Harddisk:
 
 	def killPartition(self, n):
 		part = self.partitionPath(n)
-		try:
-			h = open(part, 'wb')
-			zero = 512 * '\0'
-			for i in range(3):
-				h.write(zero)
-			h.close()
-		except Exception, e:
-			print "[Harddisk] Failed to write to", part, "error:", e
+		h = open(part, 'wb')
+		zero = 512 * '\0'
+		for i in range(3):
+			h.write(zero)
+		h.close()
 
 	errorList = [ _("Everything is fine"), _("Creating partition failed"), _("Mkfs failed"), _("Mount failed"), _("Create movie folder failed"), _("Fsck failed"), _("Please Reboot"), _("Filesystem contains uncorrectable errors"), _("Unmount failed")]
 
 	def createInitializeJob(self):
 		job = Task.Job(_("Initializing storage device..."))
 
-		task = Task.PythonTask(job, _("Unmount"))
-		task.work = self.unmount
+		task =  UnmountTask(job, self)
 
 		task = Task.PythonTask(job, _("Kill partition"))
 		task.work = lambda: self.killPartition("1")
@@ -285,8 +281,7 @@ class Harddisk:
 			task.args += ["-T", "largefile", "-N", str(size * 32)]
 		task.args += ["-m0", "-O", "dir_index", self.partitionPath("1")]
 
-		task = Task.PythonTask(job, _("Mount"))
-		task.work = self.mount
+		task = MountTask(job, self)
 
 		return job
 
@@ -337,8 +332,7 @@ class Harddisk:
 		job = Task.Job(_("Checking Filesystem..."))
 		if self.findMount():
 			# Create unmount task if it was not mounted
-			task = Task.PythonTask(job, _("Unmount"))
-			task.work = self.unmount
+			UnmountTask(job, self)
 			dev = self.mount_device
 		else:
 			# otherwise, assume there is one partition
@@ -350,8 +344,7 @@ class Harddisk:
 		task.args.append('-p')
 		task.args.append(dev)
 
-		task = Task.PythonTask(job, _("Mount"))
-		task.work = self.mount
+                MountTask(job, self)
 
 		return job
 
@@ -690,5 +683,42 @@ class HarddiskManager:
 			if x.mountpoint == mountpoint:
 				self.partitions.remove(x)
 				self.on_partition_list_change("remove", x)
+
+class UnmountTask(Task.LoggingTask):
+	def __init__(self, job, hdd):
+		Task.LoggingTask.__init__(self, job, _("Unmount"))
+		self.hdd = hdd
+	def prepare(self):
+		dev = self.hdd.mountDevice()
+		if dev:
+			self.setCmdline('umount ' + dev)
+			self.postconditions.append(Task.ReturncodePostcondition())
+
+class MountTask(Task.LoggingTask):
+	def __init__(self, job, hdd):
+		Task.LoggingTask.__init__(self, job, _("Mount"))
+		self.hdd = hdd
+	def prepare(self):
+		# try mounting through fstab first
+		if self.hdd.mount_device is None:
+			dev = self.hdd.partitionPath("1")
+		else:
+			# if previously mounted, use the same spot
+			dev = self.hdd.mount_device
+		fstab = open("/etc/fstab")
+		lines = fstab.readlines()
+		fstab.close()
+		for line in lines:
+			parts = line.strip().split(" ")
+			fspath = path.realpath(parts[0])
+			if path.realpath(fspath) == dev:
+				self.setCmdline("mount -t ext3 " + fspath)
+				self.postconditions.append(Task.ReturncodePostcondition())
+				return
+		# device is not in fstab
+		if self.hdd.type == self.hdd.DEVTYPE_UDEV:
+			# we can let udev do the job, re-read the partition table
+			self.setCmdline('sfdisk -R ' + self.hdd.disk_path)
+       			self.postconditions.append(Task.ReturncodePostcondition())
 
 harddiskmanager = HarddiskManager()

commit 384c23703c462c0531a9898f41d344fc0fbfd156

Task.py: Fix PythonTask
broken since 19ed9ee4c17df5b2823efd88c77f17aa30dccc22

lib/python/Components/Task.py
@@ -160,6 +160,9 @@ class Task(object):
 		return not_met
 
 	def _run(self):
+		if (self.cmd is None) and (self.cmdline is None):
+			self.finish()
+			return
 		from enigma import eConsoleAppContainer
 		self.container = eConsoleAppContainer()
 		self.container.appClosed.append(self.processFinished)
@@ -179,16 +182,15 @@ class Task(object):
 	def run(self, callback):
 		failed_preconditions = self.checkPreconditions(True) + self.checkPreconditions(False)
 		if failed_preconditions:
+			print "[Task] preconditions failed"
 			callback(self, failed_preconditions)
 			return
 		self.callback = callback
 		try:
 			self.prepare()
-			if (self.cmd is None) and (self.cmdline is None):
-				self.finish()
-			else: 
-				self._run()
+			self._run()
 		except Exception, ex:
+			print "[Task] exception:", ex
 			self.postconditions = [FailedPostcondition(ex)]
 			self.finish()
 