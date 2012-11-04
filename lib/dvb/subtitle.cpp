openhdf/enigma2

commit ae0e0d5888eb8b83628a582030b4b87df5b9ac21

Fix a bunch of compiler warnings
Mostly unused vars and unused results

lib/dvb/subtitle.cpp
@@ -518,9 +518,7 @@ int eDVBSubtitleParser::subtitle_process_segment(__u8 *segment)
 		memset(region->buffer->surface->data, 0, region->height * region->buffer->surface->stride);
 //		eDebug("new buffer %p", &(*region->buffer));
 
-		int region_level_of_compatibility, depth;
-
-		region_level_of_compatibility = (*segment >> 5) & 7;
+		int depth;
 		depth = (*segment++ >> 2) & 7;
 
 		region->depth = (subtitle_region::tDepth) depth;
@@ -710,15 +708,14 @@ int eDVBSubtitleParser::subtitle_process_segment(__u8 *segment)
 	}
 	case 0x13: // object data segment
 	{
-		int object_id, object_version_number, object_coding_method, non_modifying_color_flag;
+		int object_id;
+		int object_coding_method;
 
 		object_id  = *segment++ << 8;
 		object_id |= *segment++;
 		processed_length += 2;
 
-		object_version_number = *segment >> 4;
 		object_coding_method  = (*segment >> 2) & 3;
-		non_modifying_color_flag = (*segment++ >> 1) & 1;
 		processed_length++;
 
 //		eDebug("object id %04x, version %d, object_coding_method %d (page_id %d)", object_id, object_version_number, object_coding_method, page_id);
@@ -879,7 +876,7 @@ void eDVBSubtitleParser::subtitle_process_pes(__u8 *pkt, int len)
 			return;
 		}
 		pkt++; len--; // data identifier
-		*pkt++; len--; // stream id;
+		pkt++; len--; // stream id;
 
 		if (len <= 0)
 		{

lib/dvb/cablescan.cpp
@@ -135,9 +135,6 @@ void eCableScan::parseNIT()
 		for (TransportStreamInfoConstIterator tsinfo(tsinfovec.begin());
 			tsinfo != tsinfovec.end(); ++tsinfo)
 		{
-			eOriginalNetworkID onid = (*tsinfo)->getOriginalNetworkId();
-			eTransportStreamID tsid = (*tsinfo)->getTransportStreamId();
-
 			for (DescriptorConstIterator desc = (*tsinfo)->getDescriptors()->begin();
 					desc != (*tsinfo)->getDescriptors()->end(); ++desc)
 			{

lib/dvb/esection.cpp
@@ -1,6 +1,8 @@
 #include <lib/dvb/esection.h>
 #include <lib/base/eerror.h>
 
+static int totalSections(int max) { return max + 1; }
+
 void eGTable::sectionRead(const __u8 *d)
 {
 	unsigned int last_section_number = d[7];

lib/dvb/frontend.cpp
@@ -1789,7 +1789,7 @@ int eDVBFrontend::tuneLoopInt()  // called by m_tuneTimer
 				else if (!memcmp(m_sec_sequence.current()->diseqc.data, "\xE0\x00\x03", 3))
 					eDebugNoSimulate("(DiSEqC peripherial power on)");
 				else
-					eDebugNoSimulate("");
+					eDebugNoSimulate("(?)");
 				++m_sec_sequence.current();
 				break;
 			case eSecCommand::SEND_TONEBURST:

lib/dvb/scan.cpp
@@ -1262,13 +1262,13 @@ RESULT eDVBScan::processSDT(eDVBNamespace dvbnamespace, const ServiceDescription
 				{
 					CaIdentifierDescriptor &d = (CaIdentifierDescriptor&)**desc;
 					const CaSystemIdList &caids = *d.getCaSystemIds();
-					SCAN_eDebugNoNewLine("CA ");
+					SCAN_eDebugNoNewLine("CA");
 					for (CaSystemIdList::const_iterator i(caids.begin()); i != caids.end(); ++i)
 					{
-						SCAN_eDebugNoNewLine("%04x ", *i);
+						SCAN_eDebugNoNewLine(" %04x", *i);
 						service->m_ca.push_front(*i);
 					}
-					SCAN_eDebug("");
+					SCAN_eDebug(".");
 					break;
 				}
 				default:

lib/dvb/sec.cpp
@@ -44,11 +44,6 @@ eDVBSatelliteEquipmentControl::eDVBSatelliteEquipmentControl(eSmartPtrList<eDVBR
 		if (!simulate) \
 			eSecDebug(x); \
 	} while(0)
-//		else \
-//		{ \
-//			eDebugNoNewLine("SIMULATE:"); \
-//			eDebug(x); \
-//		} \
 
 int eDVBSatelliteEquipmentControl::canTune(const eDVBFrontendParametersSatellite &sat, iDVBFrontend *fe, int slot_id, int *highest_score_lnb)
 {
@@ -286,11 +281,6 @@ bool need_turn_fast(int turn_speed)
 		if (!simulate) \
 			eDebug(x); \
 	} while(0)
-//		else \
-//		{ \
-//			eDebugNoNewLine("SIMULATE:"); \
-//			eDebug(x); \
-//		} \
 
 RESULT eDVBSatelliteEquipmentControl::prepare(iDVBFrontend &frontend, const eDVBFrontendParametersSatellite &sat, int &frequency, int slot_id, unsigned int tunetimeout)
 {

lib/dvb/teletext.cpp
@@ -231,7 +231,7 @@ void eDVBTeletextParser::processPESPacket(__u8 *pkt, int len)
 	
 	while (len > 2)
 	{
-		/*unsigned char data_unit_id = */*p++;
+		p++; /* data_unit_id */
 		unsigned char data_unit_length = *p++;
 		len -= 2;
 		
@@ -253,7 +253,7 @@ void eDVBTeletextParser::processPESPacket(__u8 *pkt, int len)
 //			break;
 //		}
 		
-		/*unsigned char line_offset =*/ *p++; len--;
+		p++; len--; /* line_offset */
 		unsigned char framing_code = *p++; len--;
 
 		int magazine_and_packet_address = decode_hamming_84(p++); len--;

lib/dvb/tstools.cpp
@@ -1,4 +1,3 @@
-#define _ISOC99_SOURCE /* for llabs */
 #include <lib/dvb/tstools.h>
 #include <lib/base/eerror.h>
 #include <lib/base/cachedtssource.h>

commit 74934de82bd9d031b3a02d6950a006a07b5a1a66

Fix DVB subtitles no longer working
Broken by ae0e0d5888eb8b83628a582030b4b87df5b9ac21
(side effect slipped through)

lib/dvb/subtitle.cpp
@@ -716,6 +716,7 @@ int eDVBSubtitleParser::subtitle_process_segment(__u8 *segment)
 		processed_length += 2;
 
 		object_coding_method  = (*segment >> 2) & 3;
+		segment++; // non_modifying_color_flag
 		processed_length++;
 
 //		eDebug("object id %04x, version %d, object_coding_method %d (page_id %d)", object_id, object_version_number, object_coding_method, page_id);