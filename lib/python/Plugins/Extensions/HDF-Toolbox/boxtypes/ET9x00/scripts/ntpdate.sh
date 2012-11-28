f510b0ccf27b2ae4f8d0d925f1b59ddccd3bbab9
Henry Schrapel <schraphy@X6.(none)>
2012-11-28 08:55:04 +0100

    Removed Toolbox

--- lib/python/Plugins/Extensions/HDF-Toolbox/boxtypes/ET9x00/scripts/ntpdate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "sync time with pool.ntp.org"
-echo
-ntpdate -u pool.ntp.org
-echo
-exit 0   
--- lib/python/Plugins/Extensions/HDF-Toolbox/boxtypes/GigaBlue/scripts/picons_del.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "remove all picons"
-rm -f /usr/share/enigma2/picon/*.*
-rmdir /usr/share/enigma2/picon
-echo "Picons removed"
-
-exit 0 
