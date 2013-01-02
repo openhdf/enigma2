581ab48a143e37e0aa514c9671bf7f7ab74e226a
Henry Schrapel <dende@hdfreaks.cc>
2013-01-02 16:27:53 +0100

    tmtwin: Removed 1080p

--- lib/python/Plugins/SystemPlugins/Videomode/VideoHardware.py
+++ lib/python/Plugins/SystemPlugins/Videomode/VideoHardware.py
@@ -53,18 +53,22 @@ class VideoHardware:
 								"60Hz":		{ 60: "1080i" },
 								"multi":	{ 50: "1080i50", 60: "1080i" } }
 
-	if chipset.find('7405') != -1 or chipset.find('7335') != -1:
-		rates["1080p"] =		{ "24Hz":		{ 24: "1080p24" },
-									"25Hz":		{ 25: "1080p25" },
-									"30Hz":		{ 30: "1080p30" }}
-
-	elif chipset.find('7358') != -1 or chipset.find('7356') != -1:
-		rates["1080p"] =		{ 	"24Hz":		{ 24: "1080p24" },
-									"25Hz":		{ 25: "1080p25" },
-									"30Hz":		{ 30: "1080p30" },
-									"50Hz":		{ 50: "1080p50" },
-									"60Hz":		{ 60: "1080p" },
-									"multi":	{ 50: "1080p50", 60: "1080p" }}
+#	if chipset.find('7405') != -1 or chipset.find('7335') != -1:
+#		rates["1080p"] =		{ "24Hz":		{ 24: "1080p24" },
+#									"25Hz":		{ 25: "1080p25" },
+#									"30Hz":		{ 30: "1080p30" }}
+#
+#	elif chipset.find('7358') != -1 or chipset.find('7356') != -1:
+#		rates["1080p"] =		{ 	"24Hz":		{ 24: "1080p24" },
+#									"25Hz":		{ 25: "1080p25" },
+#									"30Hz":		{ 30: "1080p30" },
+#									"50Hz":		{ 50: "1080p50" },
+#									"60Hz":		{ 60: "1080p" },
+#									"multi":	{ 50: "1080p50", 60: "1080p" }}
+	if chipset.find('7358') != -1 or chipset.find('7356') != -1:
+		rates["1080p"] =	{ 	"50Hz":		{ 50: "1080p50" },
+								"60Hz":		{ 60: "1080p" },
+								"multi":	{ 50: "1080p50", 60: "1080p" }}
 
 	rates["PC"] = {
 		"1024x768": { 60: "1024x768" }, # not possible on DM7025
@@ -85,7 +89,7 @@ class VideoHardware:
 	modes["Scart"] = ["PAL", "NTSC", "Multi"]
 	modes["DVI-PC"] = ["PC"]
 
-	if  chipset.find('7335') != -1 or chipset.find('7358') != -1 or chipset.find('7356') != -1 or chipset.find('7405') != -1:
+	if  chipset.find('7358') != -1 or chipset.find('7356') != -1 or chipset.find('7405') != -1:
 		modes["YPbPr"] = ["720p", "1080i", "1080p", "576p", "480p", "576i", "480i"]
 		modes["DVI"] = ["720p", "1080i", "1080p", "576p", "480p", "576i", "480i"]
 		widescreen_modes = set(["720p", "1080i", "1080p"])
