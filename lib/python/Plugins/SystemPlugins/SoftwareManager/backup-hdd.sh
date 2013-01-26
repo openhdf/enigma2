48f2646381f321538c285742f2638f0208819fb9
koivo <koivo@live.de>
2013-01-26 13:48:42 +0100

    remove opkg list before backup

--- lib/python/Plugins/SystemPlugins/SoftwareManager/backup-hdd.sh
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/backup-hdd.sh
@@ -2,9 +2,12 @@ echo "HDD-CHECK" > /hdd/hdd-check;
 
 if [ -f /hdd/hdd-check ] ; then
 	echo "Full back-up to the harddisk"
-        /usr/lib/enigma2/python/Plugins/SystemPlugins/SoftwareManager/backup.sh /hdd | tee /tmp/HDD-Full-Backup.log 
+		##remove opkg lists
+		rm -rf /var/lib/opkg/lists/
+		sync
+		/usr/lib/enigma2/python/Plugins/SystemPlugins/SoftwareManager/backup.sh /hdd | tee /tmp/HDD-Full-Backup.log 
 	rm -f /hdd/hdd-check
-        wait 3
+		wait 3
 else
 	echo "---> I M A G E  C R E A T I O N  F A I L E D! <---"
     echo "\nProbable causes could be: "
