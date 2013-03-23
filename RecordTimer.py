ab4cf7445cfdb9e13911c65a55f3c19005681e45
Koivo <koivo@live.de>
2013-03-23 13:33:44 +0100

    fix blackscreen at timer add and remove

--- RecordTimer.py
+++ RecordTimer.py
@@ -133,7 +133,10 @@ class RecordTimerEntry(timer.TimerEntry, object):
 		self.resetState()
 
 	def __repr__(self):
-		return "RecordTimerEntry(name=%s, begin=%s, serviceref=%s, justplay=%s)" % (self.name, ctime(self.begin), self.service_ref, self.justplay)
+		if not self.disabled:
+			return "RecordTimerEntry(name=%s, begin=%s, serviceref=%s, justplay=%s)" % (self.name, ctime(self.begin), self.service_ref, self.justplay)
+		else:
+			return "RecordTimerEntry(name=%s, begin=%s, serviceref=%s, justplay=%s, Disabled)" % (self.name, ctime(self.begin), self.service_ref, self.justplay)
 
 	def log(self, code, msg):
 		self.log_entries.append((int(time()), code, msg))
@@ -169,34 +172,24 @@ class RecordTimerEntry(timer.TimerEntry, object):
 		service_name = self.service_ref.getServiceName()
 		begin_date = strftime("%Y%m%d %H%M", localtime(self.begin))
 
-		print "begin_date: ", begin_date
-		print "service_name: ", service_name
-		print "name:", self.name
-		print "description: ", self.description
-
+#		print "begin_date: ", begin_date
+#		print "service_name: ", service_name
+#		print "name:", self.name
+#		print "description: ", self.description
+#
 		filename = begin_date + " - " + service_name
 		if self.name:
-			if config.recording.filename_composition.value == "short":
+			if config.recording.filename_composition.getValue() == "short":
 				filename = strftime("%Y%m%d", localtime(self.begin)) + " - " + self.name
-			elif config.recording.filename_composition.value == "long":
+			elif config.recording.filename_composition.getValue() == "long":
 				filename += " - " + self.name + " - " + self.description
 			else:
 				filename += " - " + self.name # standard
 
-		if config.recording.ascii_filenames.value:
+		if config.recording.ascii_filenames.getValue():
 			filename = ASCIItranslit.legacyEncode(filename)
 
-
-		if not self.dirname:
-			dirname = findSafeRecordPath(defaultMoviePath())
-		else:
-			dirname = findSafeRecordPath(self.dirname)
-			if dirname is None:
-				dirname = findSafeRecordPath(defaultMoviePath())
-				self.dirnameHadToFallback = True
-		if not dirname:
-			return None
-		self.Filename = Directories.getRecordingFilename(filename, dirname)
+		self.Filename = Directories.getRecordingFilename(filename, self.MountPath)
 		self.log(0, "Filename calculated as: '%s'" % self.Filename)
 		return self.Filename
 
@@ -471,7 +464,7 @@ class RecordTimerEntry(timer.TimerEntry, object):
 		self.start_prepare = self.begin - self.prepare_time
 		self.backoff = 0
 
-		if int(old_prepare) != int(self.start_prepare):
+		if int(old_prepare) >0 and int(old_prepare) != int(self.start_prepare):
 			self.log(15, "record time changed, start prepare is now: %s" % ctime(self.start_prepare))
 
 	def gotRecordEvent(self, record, event):
