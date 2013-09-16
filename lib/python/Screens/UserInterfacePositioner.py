dfac8f0723d805fbc8a20d73fbeb6b4aa9deb8da
Koivo <koivo@live.de>
2013-09-16 05:49:02 +0200

    working version of osd position setup

--- lib/python/Screens/UserInterfacePositioner.py
+++ lib/python/Screens/UserInterfacePositioner.py
@@ -1,124 +1,13 @@
 from Screens.Screen import Screen
 from Components.ActionMap import ActionMap
-from Components.config import config, configfile, ConfigSubsection, ConfigSelectionNumber, ConfigSelection, ConfigSlider, ConfigYesNo, getConfigListEntry
+from Components.config import config, configfile, getConfigListEntry
 from Components.ConfigList import ConfigListScreen
 from Components.SystemInfo import SystemInfo
 from Components.Sources.StaticText import StaticText
 from Components.Pixmap import Pixmap
-from Components.Label import Label
-from os import path, access, R_OK
-from enigma import getDesktop, getBoxType
-
-class UserInterfacePositioner(Screen, ConfigListScreen):
-	if (getDesktop(0).size().width() == 1280):
-		skin = """
-			<screen position="center,center" size="1280,720" backgroundColor="#000000" title="OSD Adjustment" >
-
-				<widget source="title" render="Label" position="200,130" zPosition="1" size="880,60" font="Regular;58" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
-				<widget name="config" position="150,250" zPosition="1" size="980,150" font="Regular;20" halign="center" valign="center" transparent="1" />
-				<widget source="info1" render="Label" position="200,450" zPosition="1" size="880,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
-				<widget source="info2" render="Label" position="200,480" zPosition="1" size="880,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
-
-				<eLabel backgroundColor="red" position="0,0" size="1280,1" zPosition="0" />
-				<eLabel backgroundColor="red" position="0,719" size="1280,1" zPosition="0" />
-				<eLabel backgroundColor="red" position="0,0" size="1,720" zPosition="0" />
-				<eLabel backgroundColor="red" position="1279,0" size="1,720" zPosition="0" />
-				<eLabel backgroundColor="green" position="25,25" size="1230,1" zPosition="0" />
-				<eLabel backgroundColor="green" position="25,694" size="1230,1" zPosition="0" />
-				<eLabel backgroundColor="green" position="25,25" size="1,670" zPosition="0" />
-				<eLabel backgroundColor="green" position="1254,25" size="1,670" zPosition="0" />
-				<eLabel backgroundColor="yellow" position="50,50" size="1180,1" zPosition="0" />
-				<eLabel backgroundColor="yellow" position="50,669" size="1180,1" zPosition="0" />
-				<eLabel backgroundColor="yellow" position="50,50" size="1,620" zPosition="0" />
-				<eLabel backgroundColor="yellow" position="1229,50" size="1,620" zPosition="0" />
-				<eLabel backgroundColor="blue" position="75,75" size="1130,1" zPosition="0" />
-				<eLabel backgroundColor="blue" position="75,644" size="1130,1" zPosition="0" />
-				<eLabel backgroundColor="blue" position="75,75" size="1,570" zPosition="0" />
-				<eLabel backgroundColor="blue" position="1204,75" size="1,570" zPosition="0" />
-
-				<eLabel backgroundColor="red" position="189,627" size="140,3" zPosition="0" />
-				<eLabel backgroundColor="green" position="443,627" size="140,3" zPosition="0" />
-				<eLabel backgroundColor="yellow" position="697,627" size="140,3" zPosition="0" />
-				<eLabel backgroundColor="blue" position="951,627" size="140,3" zPosition="0" />
-				<widget source="key_red" render="Label" position="189,605" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
-				<widget source="key_green" render="Label" position="443,605" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
-				<widget source="key_yellow" render="Label" position="697,605" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
-				<widget source="key_blue" render="Label" position="951,605" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
-
-			</screen>"""
-
-	elif (getDesktop(0).size().width() == 1024):
-		skin = """
-			<screen position="center,center" size="1024,576" backgroundColor="#000000" title="OSD Adjustment" >
-
-				<widget source="title" render="Label" position="200,100" zPosition="1" size="624,40" font="Regular;38" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
-				<widget source="config" render="Label" position="100,180" zPosition="1" size="824,50" font="Regular;24" halign="center" valign="center" transparent="1" />
-				<widget source="info1" render="Label" position="200,450" zPosition="1" size="624,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
-				<widget source="info2" render="Label" position="200,480" zPosition="1" size="624,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
-
-				<eLabel backgroundColor="red" position="0,0" size="1024,1" zPosition="0" />
-				<eLabel backgroundColor="red" position="0,575" size="1024,1" zPosition="0" />
-				<eLabel backgroundColor="red" position="0,0" size="1,576" zPosition="0" />
-				<eLabel backgroundColor="red" position="1023,0" size="1,576" zPosition="0" />
-				<eLabel backgroundColor="green" position="25,25" size="974,1" zPosition="0" />
-				<eLabel backgroundColor="green" position="25,551" size="974,1" zPosition="0" />
-				<eLabel backgroundColor="green" position="25,25" size="1,526" zPosition="0" />
-				<eLabel backgroundColor="green" position="999,25" size="1,526" zPosition="0" />
-				<eLabel backgroundColor="yellow" position="50,50" size="924,1" zPosition="0" />
-				<eLabel backgroundColor="yellow" position="50,526" size="924,1" zPosition="0" />
-				<eLabel backgroundColor="yellow" position="50,50" size="1,476" zPosition="0" />
-				<eLabel backgroundColor="yellow" position="974,50" size="1,476" zPosition="0" />
-				<eLabel backgroundColor="blue" position="75,75" size="874,1" zPosition="0" />
-				<eLabel backgroundColor="blue" position="75,501" size="874,1" zPosition="0" />
-				<eLabel backgroundColor="blue" position="75,75" size="1,426" zPosition="0" />
-				<eLabel backgroundColor="blue" position="949,75" size="1,426" zPosition="0" />
-
-				<eLabel backgroundColor="red" position="138,477" size="140,3" zPosition="0" />
-				<eLabel backgroundColor="green" position="341,477" size="140,3" zPosition="0" />
-				<eLabel backgroundColor="yellow" position="544,477" size="140,3" zPosition="0" />
-				<eLabel backgroundColor="blue" position="747,477" size="140,3" zPosition="0" />
-				<widget source="key_red" render="Label" position="138,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
-				<widget source="key_green" render="Label" position="341,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
-				<widget source="key_yellow" render="Label" position="544,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
-				<widget source="key_blue" render="Label" position="747,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
-			</screen>"""
-
-	else:
-		skin = """
-			<screen position="center,center" size="720,576" backgroundColor="#000000" title="OSD Adjustment" >
-
-				<widget source="title" render="Label" position="75,100" zPosition="1" size="570,40" font="Regular;38" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
-				<widget source="config" render="Label" position="75,180" zPosition="1" size="570,50" font="Regular;21" halign="center" valign="center" transparent="1" />
-				<widget source="info1" render="Label" position="75,450" zPosition="1" size="570,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
-				<widget source="info2" render="Label" position="75,480" zPosition="1" size="570,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
-
-				<eLabel backgroundColor="red" position="0,0" size="720,1" zPosition="0" />
-				<eLabel backgroundColor="red" position="0,575" size="720,1" zPosition="0" />
-				<eLabel backgroundColor="red" position="0,0" size="1,576" zPosition="0" />
-				<eLabel backgroundColor="red" position="719,0" size="1,576" zPosition="0" />
-				<eLabel backgroundColor="green" position="25,25" size="670,1" zPosition="0" />
-				<eLabel backgroundColor="green" position="25,551" size="670,1" zPosition="0" />
-				<eLabel backgroundColor="green" position="25,25" size="1,526" zPosition="0" />
-				<eLabel backgroundColor="green" position="694,25" size="1,526" zPosition="0" />
-				<eLabel backgroundColor="yellow" position="50,50" size="620,1" zPosition="0" />
-				<eLabel backgroundColor="yellow" position="50,526" size="620,1" zPosition="0" />
-				<eLabel backgroundColor="yellow" position="50,50" size="1,476" zPosition="0" />
-				<eLabel backgroundColor="yellow" position="670,50" size="1,476" zPosition="0" />
-				<eLabel backgroundColor="blue" position="75,75" size="570,1" zPosition="0" />
-				<eLabel backgroundColor="blue" position="75,501" size="570,1" zPosition="0" />
-				<eLabel backgroundColor="blue" position="75,75" size="1,426" zPosition="0" />
-				<eLabel backgroundColor="blue" position="645,75" size="1,426" zPosition="0" />
-
-				<eLabel backgroundColor="red" position="80,477" size="140,3" zPosition="0" />
-				<eLabel backgroundColor="green" position="220,477" size="140,3" zPosition="0" />
-				<eLabel backgroundColor="yellow" position="360,477" size="140,3" zPosition="0" />
-				<eLabel backgroundColor="blue" position="500,477" size="140,3" zPosition="0" />
-				<widget source="key_red" render="Label" position="80,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
-				<widget source="key_green" render="Label" position="220,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
-				<widget source="key_yellow" render="Label" position="360,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
-				<widget source="key_blue" render="Label" position="500,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
-
-			</screen>"""
+from Components.Console import Console
+from enigma import getDesktop
+from os import access, R_OK
 
 def InitOsd():
 	SystemInfo["CanChange3DOsd"] = (access('/proc/stb/fb/3dmode', R_OK) or access('/proc/stb/fb/primary/3d', R_OK)) and True or False
@@ -187,14 +76,11 @@ class UserInterfacePositioner(Screen, ConfigListScreen):
 	def __init__(self, session):
 		Screen.__init__(self, session)
 		self.setup_title = _("Position Setup")
+		self.Console = Console()
 		self["status"] = StaticText()
 		self["key_red"] = StaticText(_("Cancel"))
-		self["key_green"] = StaticText(_("Save"))
+		self["key_green"] = StaticText(_("OK"))
 		self["key_yellow"] = StaticText(_("Defaults"))
-
-		self["title"] = StaticText(_("OSD Adjustment"))
-		self["info1"] = StaticText(_("Use arrows Up/Down to select"))
-		self["info2"] = StaticText(_("Use arrows Left/Right to adjust"))
 
 		self["actions"] = ActionMap(["SetupActions", "ColorActions"],
 			{
@@ -211,7 +97,7 @@ class UserInterfacePositioner(Screen, ConfigListScreen):
 		if SystemInfo["CanChangeOsdAlpha"] == True:
 			self.list.append(getConfigListEntry(_("User interface visibility"), config.osd.alpha, _("This option lets you adjust the transparency of the user interface")))
 		if SystemInfo["CanChangeOsdPosition"] == True:
-			self.list.append(getConfigListEntry(_("Move Left/Right"), config.osd.dst_left, _("Use the Left/Right buttons on your remote to move the user interface left/right")))
+			self.list.append(getConfigListEntry(_("Move Left/Right"), config.osd.dst_left, _("Use the Left/Right buttons on your remote to move the user inyterface left/right")))
 			self.list.append(getConfigListEntry(_("Width"), config.osd.dst_width, _("Use the Left/Right buttons on your remote to adjust the size of the user interface. Left button decreases the size, Right increases the size.")))
 			self.list.append(getConfigListEntry(_("Move Up/Down"), config.osd.dst_top, _("Use the Left/Right buttons on your remote to move the user interface up/down")))
 			self.list.append(getConfigListEntry(_("Height"), config.osd.dst_height, _("Use the Left/Right buttons on your remote to adjust the size of the user interface. Left button decreases the size, Right increases the size.")))
@@ -228,6 +114,7 @@ class UserInterfacePositioner(Screen, ConfigListScreen):
 
 	def layoutFinished(self):
 		self.setTitle(_(self.setup_title))
+		self.Console.ePopen('/usr/bin/showiframe /usr/share/enigma2/hd-testcard.mvi')
 
 	def createSummary(self):
 		from Screens.Setup import SetupSummary
@@ -314,55 +201,17 @@ class UserInterfacePositioner(Screen, ConfigListScreen):
 		configfile.save()
 		self.close()
 
-def setPosition(dst_left, dst_width, dst_top, dst_height):
-	try:
-		print 'Setting OSD position:' + str(dst_left) + " " + str(dst_width) + " " + str(dst_top) + " " + str(dst_height)
-		f = open("/proc/stb/fb/dst_left", "w")
-		f.write('%X' % int(dst_left))
-		f.close()
-		f = open("/proc/stb/fb/dst_width", "w")
-		f.write('%X' % int(dst_width))
-		f.close()
-		f = open("/proc/stb/fb/dst_top", "w")
-		f.write('%X' % int(dst_top))
-		f.close()
-		f = open("/proc/stb/fb/dst_height", "w")
-		f.write('%X' % int(dst_height))
-		f.close()
-	except:
-		return
-
-def setAlpha(alpha_value):
-	try:
-		print 'Setting OSD alpha:', str(alpha_value)
-		f = open("/proc/stb/video/alpha", "w")
-		f.write(str(alpha_value))
-		f.close()
-	except:
-		return
-
 class OSD3DSetupScreen(Screen, ConfigListScreen):
-	skin = """
-	<screen position="c-200,c-100" size="400,200" title="OSD 3D setup">
-		<widget name="config" position="c-175,c-75" size="350,150" />
-		<ePixmap pixmap="skin_default/buttons/red.png" position="c-145,e-45" zPosition="0" size="140,40" alphatest="on" />
-		<ePixmap pixmap="skin_default/buttons/green.png" position="c+5,e-45" zPosition="0" size="140,40" alphatest="on" />
-		<widget source="key_red" render="Label" position="c-145,e-45" size="140,40" valign="center" halign="center" zPosition="1" font="Regular;20" transparent="1" backgroundColor="red" />
-		<widget source="key_green" render="Label" position="c+5,e-45" size="140,40" valign="center" halign="center" zPosition="1" font="Regular;20" transparent="1" backgroundColor="green" />
-	</screen>"""
-
 	def __init__(self, session):
-		self.skin = OSD3DSetupScreen.skin
 		Screen.__init__(self, session)
 		self.setup_title = _("OSD 3D Setup")
 		self.skinName = "Setup"
 		self["status"] = StaticText()
 		self["HelpWindow"] = Pixmap()
 		self["HelpWindow"].hide()
+
 		self["key_red"] = StaticText(_("Cancel"))
 		self["key_green"] = StaticText(_("OK"))
-		self["info1"] = Label(_("Use arrows Up/Down to select"))
-		self["info2"] = Label(_("Use arrows Left/Right to adjust"))
 
 		self["actions"] = ActionMap(["SetupActions"],
 			{
@@ -375,8 +224,6 @@ class OSD3DSetupScreen(Screen, ConfigListScreen):
 		ConfigListScreen.__init__(self, self.list, session = self.session, on_change = self.changedEntry)
 		self.list.append(getConfigListEntry(_("3D Mode"), config.osd.threeDmode, _("This option lets you choose the 3D mode")))
 		self.list.append(getConfigListEntry(_("Depth"), config.osd.threeDznorm, _("This option lets you adjust the 3D depth")))
-		if getBoxType() == 'gb800se' or getBoxType() == 'gb800solo' or getBoxType() == 'gb800ue':
-			self.list.append(getConfigListEntry(_("Set Mode"), config.osd.threeDsetmode, _("This option lets you adjust the 3D mode")))
 		self.list.append(getConfigListEntry(_("Show in extensions list ?"), config.osd.show3dextensions, _("This option lets you show the option in the extension screen")))
 		self["config"].list = self.list
 		self["config"].l.setList(self.list)
@@ -387,7 +234,7 @@ class OSD3DSetupScreen(Screen, ConfigListScreen):
 		self.selectionChanged()
 
 	def selectionChanged(self):
- 		self["status"].setText(self["config"].getCurrent()[2])
+		self["status"].setText(self["config"].getCurrent()[2])
 
 	def layoutFinished(self):
 		self.setTitle(_(self.setup_title))
@@ -432,77 +279,3 @@ class OSD3DSetupScreen(Screen, ConfigListScreen):
 			self.session.openWithCallback(self.cancelConfirm, MessageBox, _("Really close without saving settings?"))
 		else:
 			self.close()
-
-def applySettings(mode, znorm):
-	try:
-		print 'Setting 3D mode:',mode
-		f = open("/proc/stb/fb/3dmode", "w")
-		f.write(mode)
-		f.close()
-		print 'Setting 3D depth:',znorm
-		f = open("/proc/stb/fb/znorm", "w")
-		f.write('%d' % znorm)
-		f.close()
-	except:
-		return
-
-def applySettings2(mode, znorm, setmode):
-	try:
-		if setmode == "mode1":
-			print 'Setting 3D mode:',mode
-			f = open("/proc/stb/fb/3dmode", "w")
-			f.write(mode)
-			f.close()
-			print 'Setting 3D depth:',znorm
-			f = open("/proc/stb/fb/znorm", "w")
-			f.write('%d' % znorm)
-			f.close()
-		elif setmode == "mode2":
-			if mode == "sidebyside" :
-				mode = "sbs"
-			elif mode == "topandbottom":
-				mode = "tab"
-			print 'Setting primary 3D mode:',mode
-			f = open("/proc/stb/fb/primary/3d", "w")
-			f.write(mode)
-			f.close()
-			print 'Setting zoffset 3D mode:',znorm
-			f = open("/proc/stb/fb/primary/zoffset", "w")
-			f.write('%d' % znorm)
-			f.close()
-	except:
-		return
-
-def setConfiguredPosition():
-	if SystemInfo["CanChangeOsdPosition"]:
-		setPosition(int(config.osd.dst_left.getValue()), int(config.osd.dst_width.getValue()), int(config.osd.dst_top.getValue()), int(config.osd.dst_height.getValue()))
-
-def setConfiguredAplha():
-	if SystemInfo["CanChangeOsdAlpha"]:
-		setAlpha(int(config.osd.alpha.getValue()))
-
-def setConfiguredSettings():
-	if SystemInfo["CanChange3DOsd"]:
-		if getBoxType() == 'gb800se' or getBoxType() == 'gb800solo' or getBoxType() == 'gb800ue':
-			applySettings2(config.osd.threeDmode.getValue(), int(config.osd.threeDznorm.getValue()), config.osd.threeDsetmode.getValue())
-		else:
-			applySettings(config.osd.threeDmode.getValue(), int(config.osd.threeDznorm.getValue()))
-
-def InitOsd():
-	try:
-		SystemInfo["CanChange3DOsd"] = (access('/proc/stb/fb/3dmode', R_OK) or access('/proc/stb/fb/primary/3d', R_OK)) and True or False
-	except:
-		SystemInfo["CanChange3DOsd"] = False
-	try:
-		SystemInfo["CanChangeOsdAlpha"] = access('/proc/stb/video/alpha', R_OK) and True or False
-	except:
-		SystemInfo["CanChangeOsdAlpha"] = False
-	try:
-		SystemInfo["CanChangeOsdPosition"] = access('/proc/stb/fb/dst_left', R_OK) and True or False
-	except:
-		SystemInfo["CanChangeOsdPosition"] = False
-	SystemInfo["OsdSetup"] = SystemInfo["CanChangeOsdPosition"]
-	if SystemInfo["CanChangeOsdAlpha"] == True or SystemInfo["CanChangeOsdPosition"] == True:
-		SystemInfo["OsdMenu"] = True
-	else:
-		SystemInfo["OsdMenu"] = False
