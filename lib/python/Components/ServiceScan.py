87496c5772cd4af3d4b76a7bcc2e495ab2c70a8d
Koivo <koivo@live.de>
2013-11-05 16:57:59 +0100

    [servicescan] fix blackscreen if exit with ok button

--- lib/python/Components/ServiceScan.py
+++ lib/python/Components/ServiceScan.py
@@ -24,6 +24,7 @@ class ServiceScan:
 
 				if errcode == 0:
 					self.state = self.Done
+					self.servicelist.listAll()
 				else:
 					self.state = self.Error
 					self.errorcode = errcode
@@ -58,7 +59,7 @@ class ServiceScan:
 							h = _("W")
 						else:
 							h = _("E")
-						if sat_name.find("%d.%d" % (orb_pos/10, orb_pos%10)) != -1:
+						if ("%d.%d" % (orb_pos/10, orb_pos%10)) in sat_name:
 							network = sat_name
 						else:
 							network = ("%s %d.%d %s") % (sat_name, orb_pos / 10, orb_pos % 10, h)
@@ -184,8 +185,9 @@ class ServiceScan:
 
 	def newService(self):
 		newServiceName = self.scan.getLastServiceName()
-		self.servicelist.addItem(newServiceName)
-		self.lcd_summary.updateService(self.scan.getLastServiceName())
+		newServiceRef = self.scan.getLastServiceRef()
+		self.servicelist.addItem((newServiceName, newServiceRef))
+		self.lcd_summary.updateService(newServiceName)
 
 	def destroy(self):
 		pass
