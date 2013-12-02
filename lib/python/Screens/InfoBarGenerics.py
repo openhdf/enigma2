523332c55a096c95fbbb467685c66b9123a468db
Koivo <koivo@live.de>
2013-12-02 20:15:53 +0100

    [infobar] fix systeminfo

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -2517,7 +2517,7 @@ class InfoBarPiP:
 			if slist and slist.dopipzap:
 				slist.togglePipzap()
 			del self.session.pip
-			if SystemInfo["LcdDisplay"]:
+			if SystemInfo["FrontpanelDisplay"]:
 				if config.lcd.modepip.value >= "1":
 					f = open("/proc/stb/lcd/mode", "w")
 					f.write(config.lcd.modeminitv.value)
@@ -2530,7 +2530,7 @@ class InfoBarPiP:
 			if self.session.pip.playService(newservice):
 				self.session.pipshown = True
 				self.session.pip.servicePath = self.servicelist.getCurrentServicePath()
-				if SystemInfo["LcdDisplay"]:
+				if SystemInfo["FrontpanelDisplay"]:
 					if config.lcd.modepip.value >= "1":
 						f = open("/proc/stb/lcd/mode", "w")
 						f.write(config.lcd.modepip.value)
