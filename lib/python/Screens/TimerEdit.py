5f89eeb8f228efda21c5757a9dc69957ae33a4f4
Koivo <koivo@live.de>
2013-12-16 18:48:22 +0100

    merge remote from openatv/enigma2

--- lib/python/Screens/TimerEdit.py
+++ lib/python/Screens/TimerEdit.py
@@ -15,8 +15,12 @@ from ServiceReference import ServiceReference
 from Screens.TimerEntry import TimerEntry, TimerLog
 from Tools.BoundFunction import boundFunction
 from Tools.FuzzyDate import FuzzyTime
-from time import time
+from Tools.Directories import resolveFilename, SCOPE_HDD, fileExists
+from time import time, localtime
 from timer import TimerEntry as RealTimerEntry
+from enigma import eServiceCenter
+import Tools.CopyFiles
+import os
 
 class TimerEditList(Screen):
 	EMPTY = 0
@@ -222,6 +226,7 @@ class TimerEditList(Screen):
 			return cmp(x[0].begin, y[0].begin)
 
 		list = self.list
+		print list
 		del list[:]
 		list.extend([(timer, False) for timer in self.session.nav.RecordTimer.timer_list])
 		list.extend([(timer, True) for timer in self.session.nav.RecordTimer.processed_timers])
@@ -251,10 +256,60 @@ class TimerEditList(Screen):
 
 	def removeTimerQuestion(self):
 		cur = self["timerlist"].getCurrent()
+		service = str(cur.service_ref.getServiceName())
+		t = localtime(cur.begin)
+		f = str(t.tm_year) + str(t.tm_mon).zfill(2) + str(t.tm_mday).zfill(2) + " " + str(t.tm_hour).zfill(2) + str(t.tm_min).zfill(2) + " - " + service + " - " + cur.name
+		f = f.replace(':','_')
+		f = f.replace(',','_')
+		f = f.replace('/','_')
+
 		if not cur:
 			return
 
-		self.session.openWithCallback(self.removeTimer, MessageBox, _("Do you really want to delete %s?") % (cur.name), default = False)
+		onhdd = False
+		self.moviename = f
+		path = resolveFilename(SCOPE_HDD)
+		try:
+			files = os.listdir(path)
+		except:
+			files = ""
+		for file in files:
+			if file.startswith(f):
+				onhdd = True
+				break
+
+		if onhdd:
+			message = (_("Do you really want to delete %s?") % (cur.name))
+			choices = [(_("No"), "no"),
+					(_("Yes, delete from Timerlist"), "yes"),
+					(_("Yes, delete from Timerlist and delete recording"), "yesremove")]
+			self.session.openWithCallback(self.startDelete, ChoiceBox, title=message, list=choices)
+		else:
+			self.session.openWithCallback(self.removeTimer, MessageBox, _("Do you really want to delete %s?") % (cur.name), default = False)
+
+	def startDelete(self, answer):
+		if not answer or not answer[1]:
+			self.close()
+			return
+		if answer[1] == 'no':
+			return
+		elif answer[1] == 'yes':
+			self.removeTimer(True)
+		elif answer[1] == 'yesremove':
+			if fileExists("/usr/lib/enigma2/python/Plugins/Extensions/EnhancedMovieCenter/plugin.pyo"):
+				if config.EMC.movie_trashcan_enable.getValue():
+					trashpath = config.EMC.movie_trashcan_path.getValue()
+					self.MoveToTrash(trashpath)
+			elif config.usage.movielist_trashcan.getValue():
+				trashpath = resolveFilename(SCOPE_HDD) + '.Trash'
+				self.MoveToTrash(trashpath)
+			else:
+				self.session.openWithCallback(self.callbackRemoveRecording, MessageBox, _("Do you really want to delete the recording?"), default = False)
+
+	def callbackRemoveRecording(self, answer):
+		if not answer:
+			return
+		self.delete()
 
 	def removeTimer(self, result):
 		if not result:
@@ -268,6 +323,34 @@ class TimerEditList(Screen):
 			self.refill()
 			self.updateState()
 
+	def MoveToTrash(self, trashpath):
+		self.removeTimer(True)
+		moviepath = os.path.normpath(resolveFilename(SCOPE_HDD))
+		movedList =[]
+		files = os.listdir(moviepath)
+		for file in files:
+			if file.startswith(self.moviename):
+				movedList.append((os.path.join(moviepath, file), os.path.join(trashpath, file)))
+		Tools.CopyFiles.moveFiles(movedList, None)
+
+	def delete(self):
+		item = self["timerlist"].getCurrent()
+		if item is None:
+			return # huh?
+		name = item.name
+		service = str(item.service_ref.getServiceName())
+		t = localtime(item.begin)
+		f = str(t.tm_year) + str(t.tm_mon).zfill(2) + str(t.tm_mday).zfill(2) + " " + str(t.tm_hour).zfill(2) + str(t.tm_min).zfill(2) + " - " + service + " - " + name
+		f = f.replace(':','_')
+		f = f.replace(',','_')
+		f = f.replace('/','_')
+		path = resolveFilename(SCOPE_HDD)
+		self.removeTimer(True)
+		from enigma import eBackgroundFileEraser
+		files = os.listdir(path)
+		for file in files:
+			if file.startswith(f):
+				eBackgroundFileEraser.getInstance().erase(os.path.realpath(path + file))
 
 	def refill(self):
 		oldsize = len(self.list)
@@ -388,7 +471,7 @@ class TimerSanityConflict(Screen):
 		self["timer2"] = TimerList(self.list2)
 
 		self["key_red"] = Button("Edit")
-# 		self["key_green"] = Button(" ")
+		self["key_green"] = Button(" ")
 		self["key_yellow"] = Button(" ")
 		self["key_blue"] = Button(" ")
 
@@ -415,6 +498,16 @@ class TimerSanityConflict(Screen):
 
 	def editTimer2(self):
 		self.session.openWithCallback(self.finishedEdit, TimerEntry, self["timer2"].getCurrent())
+
+	def toggleNewTimer(self):
+		if self.timer[0].disabled:
+			self.timer[0].disabled = False
+			self.session.nav.RecordTimer.timeChanged(self.timer[0])
+
+		elif not self.timer[0].isRunning():
+			self.timer[0].disabled = True
+			self.session.nav.RecordTimer.timeChanged(self.timer[0])
+		self.finishedEdit((True, self.timer[0]))
 
 	def toggleTimer(self):
 		x = self["list"].getSelectedIndex() + 1 # the first is the new timer so we do +1 here
@@ -456,19 +549,19 @@ class TimerSanityConflict(Screen):
 			del actions[descr]
 
 	def updateState(self):
-# 		if self.timer[0] is not None:
-# 			if self.timer[0].disabled and self.key_green_choice != self.ENABLE:
-# 				self["actions"].actions.update({"green":self.toggleTimer})
-# 				self["key_green"].setText(_("Enable"))
-# 				self.key_green_choice = self.ENABLE
-# 			elif self.timer[0].isRunning() and not self.timer[0].repeated and self.key_green_choice != self.EMPTY:
-# 				self.removeAction("green")
-# 				self["key_green"].setText(" ")
-# 				self.key_green_choice = self.EMPTY
-# 			elif (not self.timer[0].isRunning() or self.timer[0].repeated ) and self.key_green_choice != self.DISABLE:
-# 				self["actions"].actions.update({"green":self.toggleTimer})
-# 				self["key_green"].setText(_("Disable"))
-# 				self.key_green_choice = self.DISABLE
+		if self.timer[0] is not None:
+			if self.timer[0].disabled and self.key_green_choice != self.ENABLE:
+				self["actions"].actions.update({"green":self.toggleTimer})
+				self["key_green"].setText(_("Enable"))
+				self.key_green_choice = self.ENABLE
+			elif self.timer[0].isRunning() and not self.timer[0].repeated and self.key_green_choice != self.EMPTY:
+				self.removeAction("green")
+				self["key_green"].setText(" ")
+				self.key_green_choice = self.EMPTY
+			elif (not self.timer[0].isRunning() or self.timer[0].repeated ) and self.key_green_choice != self.DISABLE:
+				self["actions"].actions.update({"green":self.toggleNewTimer})
+				self["key_green"].setText(_("Disable"))
+				self.key_green_choice = self.DISABLE
 
 		if len(self.timer) > 1:
 			x = self["list"].getSelectedIndex() + 1 # the first is the new timer so we do +1 here
