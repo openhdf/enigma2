ea216f509f75dab89ab617ea3044845425d907ae
Koivo <koivo@live.de>
2014-01-17 22:29:17 +0100

    [usageconfig] fix typo

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -575,7 +575,7 @@ def InitUsageConfig():
 		("hrv", _("Croatian")),
 		("ces cze", _("Czech")),
 		("dan", _("Danish")),
-		("dut ndl Deutsch", _("Dutch")),
+		("dut ndl Dutch", _("Dutch")),
 		("eng qaa Englisch", _("English")),
 		("est", _("Estonian")),
 		("fin", _("Finnish")),
