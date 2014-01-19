606feef16905b3956e08c2ec4a5a5a4a892fcc8c
Koivo <koivo@live.de>
2014-01-19 13:13:15 +0100

    fix some enigma imports

--- lib/python/Plugins/SystemPlugins/SoftwareManager/BackupRestore.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/BackupRestore.py
@@ -16,7 +16,8 @@ from Components.ConfigList import ConfigList,ConfigListScreen
 from Components.FileList import MultiFileSelectList
 from Components.Network import iNetwork
 from Plugins.Plugin import PluginDescriptor
-from enigma import eTimer, eEnv, eConsoleAppContainer, getBoxType
+from enigma import eTimer, eEnv, eConsoleAppContainer
+from boxbranding import getBoxType, getMachineBrand, getMachineName
 from Tools.Directories import *
 from os import system, popen, path, makedirs, listdir, access, stat, rename, remove, W_OK, R_OK
 from time import gmtime, strftime, localtime, sleep
