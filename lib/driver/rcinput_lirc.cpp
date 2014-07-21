5966a7adaf8f5081dae028fcef4ed3457ef7f67a
Oliver Kleinecke <okleinecke@web.de>
2014-07-22 01:13:21 +0200

    Update rcinput_lirc.cpp

--- lib/driver/rcinput_lirc.cpp
+++ lib/driver/rcinput_lirc.cpp
@@ -530,8 +530,8 @@ void eLircInputDriver::thread()
 	cTimeMs FirstTime;
 	cTimeMs LastTime;
 	char buf[LIRC_BUFFER_SIZE];
-	char LastKeyName[54];
-	bool repeat;
+	char LastKeyName[54] = "0";
+	bool repeat = false;
 	int timeout = -1;
 	lircEvent event;
 
