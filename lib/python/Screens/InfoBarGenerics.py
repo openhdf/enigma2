13de3855cb116239ca62ba247da40edb3fc54d76
Koivo <koivo@live.de>
2014-07-21 18:30:49 +0200

    [Update] InfobarTimeshiftState - if seeking will Infobar/Seekbar not hidden

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -3362,6 +3362,7 @@ class InfoBarTimeshiftState(InfoBarPVRState):
 			if config.timeshift.showinfobar.value:
 				self["TimeshiftSeekPointerActions"].setEnabled(True)
 			self.pvrStateDialog.show()
+		if not self.isSeekable():
 			self.startHideTimer()
 
 	def __hideTimeshiftState(self):
