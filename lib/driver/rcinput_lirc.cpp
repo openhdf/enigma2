77411fabfa76bfea52b9e5789a321a94fc229172
Oliver Kleinecke <okleinecke@web.de>
2014-07-21 23:16:45 +0200

    Update rcinput_lirc.cpp

--- lib/driver/rcinput_lirc.cpp
+++ lib/driver/rcinput_lirc.cpp
@@ -12,57 +12,6 @@
 #include <lib/base/condVar.h>
 #include <lib/driver/input_fake.h>
 
-// Converts a hexadecimal string to integer
-
-int xtoi(const char* xs, unsigned int* result)
-{
- size_t szlen = strlen(xs);
- int i, xv, fact;
-
- if (szlen > 0)
- {
-  // Converting more than 32bit hexadecimal value?
-  if (szlen>8) return 2; // exit
-
-  // Begin conversion here
-  *result = 0;
-  fact = 1;
-
-  // Run until no more character to convert
-  for(i=szlen-1; i>=0 ;i--)
-  {
-   if (isxdigit(*(xs+i)))
-   {
-    if (*(xs+i)>=97)
-    {
-     xv = ( *(xs+i) - 97) + 10;
-    }
-    else if ( *(xs+i) >= 65)
-    {
-     xv = (*(xs+i) - 65) + 10;
-    }
-    else
-    {
-     xv = *(xs+i) - 48;
-    }
-    *result += (xv * fact);
-    fact *= 16;
-   }
-   else
-   {
-    // Conversion was abnormally terminated
-    // by non hexadecimal digit, hence
-    // returning only the converted with
-    // an error value 4 (illegal hex character)
-    return 4;
-   }
-  }
- }
-
- // Nothing to convert
- return 1;
-}
-
 static tKey keyTable[] = { // "Up" and "Down" must be the first two keys!
 	{ KEY_RESERVED,         "KEY_RESERVED"         },
 	{ KEY_ESC,              "KEY_ESC"              },
