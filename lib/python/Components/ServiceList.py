b89d21b4919ca4ee0d428be64bdb5008da960ba5
Koivo <koivo@live.de>
2014-08-30 17:54:13 +0200

    Correctly revert to current tv or radio bouquet
    When e.g. we are in TV mode and the service is not available in any
    bouquet then the TV mode bouquet was reverted and the radio bouquets were
    focused to the latest radio bouquet. Now we always revert to the correct
    tv or radio bouquet.
    merge remote from openpli

--- lib/python/Components/ServiceList.py
+++ lib/python/Components/ServiceList.py
@@ -152,6 +152,7 @@ class ServiceList(HTMLComponent, GUIComponent):
 				revert_mode = config.servicelist.lastmode.value
 				revert_root = self.getRoot()
 				self.serviceList.setTvMode()
+				revert_tv_root = self.getRoot()
 				bouquets = self.serviceList.getBouquetList()
 				for bouquet in bouquets:
 					self.serviceList.enterUserbouquet(bouquet[1])
@@ -159,7 +160,9 @@ class ServiceList(HTMLComponent, GUIComponent):
 						config.servicelist.lastmode.save()
 						self.serviceList.saveChannel(ref)
 						return True
-				self.serviceList.setRadioMode()
+				self.serviceList.enterUserbouquet(revert_tv_root)	
+				self.serviceList.setModeRadio()
+				revert_radio_root = self.getRoot()
 				bouquets = self.serviceList.getBouquetList()
 				for bouquet in bouquets:
 					self.serviceList.enterUserbouquet(bouquet[1])
@@ -167,6 +170,7 @@ class ServiceList(HTMLComponent, GUIComponent):
 						config.servicelist.lastmode.save()
 						self.serviceList.saveChannel(ref)
 						return True
+				self.serviceList.enterUserbouquet(revert_radio_root)		
 				print "[servicelist] service not found in any userbouquets"
 				if revert_mode == "tv":
 					self.serviceList.setModeTv()
