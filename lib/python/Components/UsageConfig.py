451fdb5fd9cfbf66293a15af062c0360c391ebf4
satinfo <satinfo76@web.de>
2014-10-14 21:55:44 +0200

    [RCU] fix RCU text input
    This will fix RCU inout on all ini hardware

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -13,7 +13,7 @@ from Tools.HardwareInfo import HardwareInfo
 
 def InitUsageConfig():
 	config.misc.useNTPminutes = ConfigSelection(default = "30", choices = [("30", "30" + " " +_("minutes")), ("60", _("Hour")), ("1440", _("Once per day"))])
-	if getBrandOEM() == 'vuplus':
+	if getBrandOEM() in ('vuplus', 'ini'):
 		config.misc.remotecontrol_text_support = ConfigYesNo(default = True)
 	else:
 		config.misc.remotecontrol_text_support = ConfigYesNo(default = False)
