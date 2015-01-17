9feb48a43fd3ce01a52411545cdb2d993ac34b69
Koivo <koivo@live.de>
2015-01-17 17:04:46 +0100

    [subtitles] hugbug/subtitles-remove-hinew setting to cleanup hearing impaired subtitles merge remote from openatv/enigma2

--- lib/python/Screens/AudioSelection.py
+++ lib/python/Screens/AudioSelection.py
@@ -490,6 +490,7 @@ class QuickSubtitlesConfigMenu(ConfigListScreen, Screen):
 				getConfigMenuItem("config.subtitles.subtitle_fontsize"),
 				getConfigMenuItem("config.subtitles.subtitle_position"),
 				getConfigMenuItem("config.subtitles.subtitle_rewrap"),
+				getConfigMenuItem("config.subtitles.pango_subtitle_removehi"),
 				getConfigMenuItem("config.subtitles.subtitle_borderwidth"),
 				getConfigMenuItem("config.subtitles.subtitle_alignment"),
 				getConfigMenuItem("config.subtitles.subtitle_bad_timing_delay"),
