8ade474c434e4434bc32490c2d477aa80dfaafb0
Koivo <koivo@live.de>
2015-01-29 07:18:51 +0100

    try to fix gbipbox image

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -116,6 +116,8 @@ eDVBResourceManager::eDVBResourceManager()
 		m_boxtype = DM800SE;
 	else if (!strncmp(tmp, "dm7020hd\n", rd))
 		m_boxtype = DM7020HD;
+	else if (!strncmp(tmp, "gbipbox\n", rd))
+		m_boxtype = GIGABLUE;
 	else {
 		eDebug("boxtype detection via /proc/stb/info not possible... use fallback via demux count!\n");
 		if (m_demux.size() == 3)
