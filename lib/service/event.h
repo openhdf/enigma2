5b89800263c0e1c70634311b5c82b9fe4c8d3f44
ims <ims21@users.sourceforge.net>
2015-03-20 13:25:04 +0100

    added getRunningStatus for event
    Signed-off-by: littlesat <littlesat99@yahoo.com>
    (cherry picked from commit 8be5f13876d3961c6512ab5dd6c7ee71573a7a6e)

--- lib/service/event.h
+++ lib/service/event.h
@@ -86,6 +86,7 @@ class eServiceEvent: public iObject
 	int m_duration;
 	int m_event_id;
 	int m_pdc_pil;
+	int m_running_status;
 	std::string m_event_name, m_short_description, m_extended_description;
 	static std::string m_language, m_language_alternative;
 	// .. additional info
