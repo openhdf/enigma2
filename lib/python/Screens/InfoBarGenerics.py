0973932ebbf987d7c5193b5b7ae23ddde4568592
Koivo <koivo@live.de>
2015-03-30 16:26:10 +0200

    merge remote from openatv/enigma2

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -630,6 +630,7 @@ class InfoBarShowHide(InfoBarScreenSaver):
 
 		self.standardInfoBar = False
 		self.lastSecondInfoBar = 0
+		self.lastResetAlpha = True
 		self.secondInfoBarScreen = ""
 		if isStandardInfoBar(self):
 			self.secondInfoBarScreen = self.session.instantiateDialog(SecondInfoBar)
@@ -677,7 +678,7 @@ class InfoBarShowHide(InfoBarScreenSaver):
 		self.doHide()
 
 	def unDimming(self):
-		self.unDimmingTimer.stop()
+		#self.unDimmingTimer.stop()
 		self.doWriteAlpha(config.av.osd_alpha.value)
 
 	def doWriteAlpha(self, value):
@@ -685,6 +686,10 @@ class InfoBarShowHide(InfoBarScreenSaver):
 			f=open("/proc/stb/video/alpha","w")
 			f.write("%i" % (value))
 			f.close()
+			if value == config.av.osd_alpha.value:
+				self.lastResetAlpha = True
+			else:
+				self.lastResetAlpha = False
 
 	def __onHide(self):
 		self.__state = self.STATE_HIDDEN
@@ -693,10 +698,11 @@ class InfoBarShowHide(InfoBarScreenSaver):
 			x(False)
 
 	def resetAlpha(self):
-		if config.usage.show_infobar_do_dimming.value:
-			self.unDimmingTimer = eTimer()
-			self.unDimmingTimer.callback.append(self.unDimming)
-			self.unDimmingTimer.start(300, True)
+		if config.usage.show_infobar_do_dimming.value and self.lastResetAlpha is False:
+			#self.unDimmingTimer = eTimer()
+			#self.unDimmingTimer.callback.append(self.unDimming)
+			#self.unDimmingTimer.start(300, True)
+			self.unDimming()
 
 	def keyHide(self):
 		if self.__state == self.STATE_HIDDEN:
