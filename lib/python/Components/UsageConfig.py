71079b607c3ff206e1fdd90702285a416e25fd30
Koivo <koivo@live.de>
2015-04-07 16:11:15 +0200

    [setup] add missing parameters

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -226,7 +226,11 @@ def InitUsageConfig():
 		("2", "DVB-C/-S/-T"),
 		("3", "DVB-C/-T/-S"),
 		("4", "DVB-T/-C/-S"),
-		("5", "DVB-T/-S/-C") ])
+		("5", "DVB-T/-S/-C"),
+		("127", "No priority") ])
+
+	config.usage.remote_fallback_enabled = ConfigYesNo(default = False);
+	config.usage.remote_fallback = ConfigText(default = "", fixed_size = False);
 
 	nims = [("-1", _("auto"))]
 	rec_nims = [("-2", _("Disabled")), ("-1", _("auto"))]
