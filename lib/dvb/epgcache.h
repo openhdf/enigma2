47f0f9297314e7fae9b38a51ccf5062f56c9a138
Koivo <koivo@live.de>
2015-04-14 11:21:55 +0200

    [epg] next try

--- lib/dvb/epgcache.h
+++ lib/dvb/epgcache.h
@@ -329,7 +329,6 @@ private:
 #endif
 	void sectionRead(const uint8_t *data, int source, channel_data *channel);
 	void gotMessage(const Message &message);
-	void flushEPG(const uniqueEPGKey & s=uniqueEPGKey());
 	void cleanLoop();
 
 // called from main thread
@@ -349,8 +348,8 @@ public:
 	void crossepgImportEPGv21(std::string dbroot);
 	void save();
 	void load();
-	void flushEPG(const uniqueEPGKey & s=uniqueEPGKey());
 	void timeUpdated();
+	void flushEPG(const uniqueEPGKey & s=uniqueEPGKey());
 #ifndef SWIG
 	eEPGCache();
 	~eEPGCache();
