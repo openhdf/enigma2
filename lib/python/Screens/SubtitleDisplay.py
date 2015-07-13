3dd38893b94f4bad05abf2747a97ce0bd53a0c51
Koivo <koivo@live.de>
2015-07-13 15:53:50 +0200

    cleanup again :D

--- lib/python/Screens/SubtitleDisplay.py
+++
@@ -1,6 +0,0 @@
-from Screens.Screen import Screen
-
-class SubtitleDisplay(Screen):
-	pass
-
-	# not really much to do...
--- lib/python/Tools/Import.py
+++
@@ -1,6 +0,0 @@
-def my_import(name):
-	mod = __import__(name)
-	components = name.split('.')
-	for comp in components[1:]:
-		mod = getattr(mod, comp)
-	return mod
