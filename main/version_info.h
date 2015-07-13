78b80abd581a1487cdf016f841f91303003783f9
Koivo <koivo@live.de>
2015-07-13 15:53:50 +0200

    cleanup again :D

--- main/version_info.h
+++
@@ -1,9 +0,0 @@
-#ifndef __main_version_info_h__
-#define __main_version_info_h__
-
-extern const char *enigma2_date;
-extern const char *enigma2_branch;
-extern const char *enigma2_rev;
-extern const char *enigma2_version;
-
-#endif
