a4e9b44f8f343fb57cdbd1319642601c04911590
Koivo <koivo@live.de>
2015-09-10 17:34:51 +0200

    [pictureplayer] update (reverted from commit 18204bfd6c920f265d422acc1f8588a7d85355ae)

--- lib/gdi/picload.h
+++ lib/gdi/picload.h
@@ -2,7 +2,6 @@
 #define __picload_h__
 
 #include <lib/gdi/gpixmap.h>
-#include <lib/gdi/picexif.h>
 #include <lib/base/thread.h>
 #include <lib/python/python.h>
 #include <lib/base/message.h>
@@ -59,7 +58,6 @@ class ePicLoad: public eMainloop, public eThread, public Object, public iObject
 	void resizePic();
 
 	Cfilepara *m_filepara;
-	Cexif *m_exif;
 	bool threadrunning;
 
 	struct PConf
@@ -67,10 +65,9 @@ class ePicLoad: public eMainloop, public eThread, public Object, public iObject
 		int max_x;
 		int max_y;
 		double aspect_ratio;
-		int background;
+		unsigned int background;
 		bool resizetype;
 		bool usecache;
-		bool auto_orientation;
 		int thumbnailsize;
 		int test;
 		PConf();
@@ -95,7 +92,6 @@ class ePicLoad: public eMainloop, public eThread, public Object, public iObject
 	void thread();
 	int startThread(int what, const char *file, int x, int y, bool async=true);
 	void thread_finished();
-	bool getExif(const char *filename);
 public:
 	void waitFinished();
 	PSignal1<void, const char*> PictureData;
@@ -106,12 +102,12 @@ public:
 	RESULT startDecode(const char *filename, int x=0, int y=0, bool async=true);
 	RESULT getThumbnail(const char *filename, int x=0, int y=0, bool async=true);
 	RESULT setPara(PyObject *val);
-	RESULT setPara(int width, int height, double aspectRatio, int as, bool useCache, int resizeType, const char *bg_str, bool auto_orientation);
+	RESULT setPara(int width, int height, double aspectRatio, int as, bool useCache, int resizeType, const char *bg_str);
 	PyObject *getInfo(const char *filename);
 	SWIG_VOID(int) getData(ePtr<gPixmap> &SWIG_OUTPUT);
 };
 
 //for old plugins
-SWIG_VOID(int) loadPic(ePtr<gPixmap> &SWIG_OUTPUT, std::string filename, int x, int y, int aspect, int resize_mode=0, int rotate=0, int background=0, std::string cachefile="");
+SWIG_VOID(int) loadPic(ePtr<gPixmap> &SWIG_OUTPUT, std::string filename, int x, int y, int aspect, int resize_mode=0, int rotate=0, unsigned int background=0, std::string cachefile="");
 
 #endif // __picload_h__
