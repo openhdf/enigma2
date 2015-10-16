590cb7858043b46c4b4574f36a3d4fd957776570
Koivo <koivo@live.de>
2015-10-16 11:54:52 +0200

    [InputDeviceSetup] add triplex

--- lib/python/Screens/InputDeviceSetup.py
+++ lib/python/Screens/InputDeviceSetup.py
@@ -271,7 +271,7 @@ class RemoteControlType(Screen, ConfigListScreen):
 			("11", _("et9200/9500/6500")),
 			("13", _("et4000")),
 			("14", _("XP1000")),
-			("16", _("HD1100/et7x00/et8500")),
+			("16", _("HD1100/HD1200/HD500C/et7x00/et8500")),
 			("17", _("XP3000")),
 			("18", _("F1/F3")),
 			("19", _("HD2400"))
@@ -282,10 +282,9 @@ class RemoteControlType(Screen, ConfigListScreen):
 			("et5000", 7),
 			("et6000", 7),
 			("et6500", 11),
-			("et7000", 16),
-			("et7500", 16),
+			("et7x00",16),
 			("et8000", 9),
-			("et8500", 16),
+			("et8500",16),
 			("et9000", 5),
 			("et9100", 5),
 			("et9200", 11),
@@ -294,8 +293,7 @@ class RemoteControlType(Screen, ConfigListScreen):
 			("formuler1", 18),
 			("formuler3", 18),
 			("xp1000", 14),
-			("hd1100", 16),
-			("hd2400", 19)
+			("xp3000", 17)
 			]
 
 	def __init__(self, session):
@@ -319,7 +317,7 @@ class RemoteControlType(Screen, ConfigListScreen):
 		self.list.append(getConfigListEntry(_("Remote control type"), self.rctype))
 		self["config"].list = self.list
 
-		self.defaultRcType = 0
+		self.defaultRcType = None
 		self.getDefaultRcType()
 
 	def getDefaultRcType(self):
