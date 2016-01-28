d5b52721e86bc639effcc34ce87e068a105223b2
Koivo <koivo@live.de>
2016-01-28 19:58:49 +0100

    [3d] merge 3d check commit from dvb-adenin (reverted from commit 89a9f5f1e0c08e12427fb30288628bf8b43c5fec)

--- lib/python/Screens/UserInterfacePositioner.py
+++ lib/python/Screens/UserInterfacePositioner.py
@@ -68,17 +68,16 @@ def InitOsd():
 			value = configElement.value
 			print 'Setting 3D mode:',value
 			try:
-				if SystemInfo["CanUse3DModeChoices"]:
-					f = open("/proc/stb/fb/3dmode_choices", "r")
-					choices = f.readlines()[0].split()
-					f.close()
-					if value not in choices:
-						if value == "sidebyside":
-							value = "sbs"
-						elif value == "topandbottom":
-							value = "tab"
-						elif value == "auto":
-							value = "off"
+				f = open("/proc/stb/fb/3dmode_choices", "r")
+				choices = f.readlines()[0].split()
+				f.close()
+				if value not in choices:
+					if value == "sidebyside":
+						value = "sbs"
+					elif value == "topandbottom":
+						value = "tab"
+					elif value == "auto":
+						value = "off"
 				f = open("/proc/stb/fb/3dmode", "w")
 				f.write(value)
 				f.close()
