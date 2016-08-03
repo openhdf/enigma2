7944cd4ac6c5f815cb6aead7393e76fa90a4fb6c
Koivo <koivo@live.de>
2016-08-03 04:58:46 +0200

    [input device] add hd51

--- lib/python/Screens/InputDeviceSetup.py
+++ lib/python/Screens/InputDeviceSetup.py
@@ -271,7 +271,7 @@ class RemoteControlType(Screen, ConfigListScreen):
 			("11", _("et9200/9500/6500")),
 			("13", _("et4000")),
 			("14", _("XP1000")),
-			("16", _("HD1100/HD1200/HD500C/et7x00/et8500")),
+			("16", _("HD51/HD1100/HD1200/HD500C/et7x00/et8500")),
 			("17", _("XP3000")),
 			("18", _("F1/F3/F4/TRIPLEX")),
 			("19", _("HD2400"))
@@ -290,6 +290,7 @@ class RemoteControlType(Screen, ConfigListScreen):
 			("et9200", 11),
 			("et9500", 11),
 			("et10000", 9),
+			("hd51",16),
 			("formuler1", 18),
 			("formuler3", 18),
 			("formuler4",18),
