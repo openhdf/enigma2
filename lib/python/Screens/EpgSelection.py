0715397cb14e22da95ba5f62e867cb8d417c1a27
iPkay <iPkay@gmx.com>
2016-10-24 15:51:21 +0200

    [EpgSelection] fix possible bsod
    http://www.opena.tv/openatv-5-3-image-rueckmeldungen/29030-gepg-taste-9-crash.html

--- lib/python/Screens/EpgSelection.py
+++ lib/python/Screens/EpgSelection.py
@@ -1376,6 +1376,7 @@ class EPGSelection(Screen, HelpableScreen):
 				self.close()
 
 	def keyNumberGlobal(self, number):
+		if self.createTimer.isActive(): return
 		if self.type == EPG_TYPE_GRAPH:
 			if number == 1:
 				timeperiod = int(config.epgselection.graph_prevtimeperiod.value)
