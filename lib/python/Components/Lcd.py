453b9545d4feef4584f51d779747a510f1896fbf
arn354 <arn354@email.de>
2017-03-18 13:21:44 +0100

    [GigaBlue] rename gbuhdquad to gbquad4k

--- lib/python/Components/Lcd.py
+++ lib/python/Components/Lcd.py
@@ -335,7 +335,7 @@ def InitLcd():
 					"5": _("PIP"),
 					"7": _("PIP with OSD")},
 					default = "0")
-			if config.misc.boxtype.value == 'gbquad' or config.misc.boxtype.value == 'gbquadplus':
+			if config.misc.boxtype.value in ( 'gbquad', 'gbquadplus', 'gbquad4k'):
 				config.lcd.modepip.addNotifier(setLCDModePiP)
 			else:
 				config.lcd.modepip = ConfigNothing()
