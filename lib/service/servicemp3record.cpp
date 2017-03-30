92b9115f287f422fc79bbd47360146b2a5928e12
Koivo <koivo@live.de>
2017-03-30 16:58:19 +0200

    [servicemp3record] add alternateUserAgent

--- lib/service/servicemp3record.cpp
+++ lib/service/servicemp3record.cpp
@@ -27,6 +27,8 @@ eServiceMP3Record::eServiceMP3Record(const eServiceReference &ref):
 
 	CONNECT(m_pump.recv_msg, eServiceMP3Record::gstPoll);
 	CONNECT(m_streamingsrc_timeout->timeout, eServiceMP3Record::sourceTimeout);
+	if (eConfigManager::getConfigBoolValue("config.mediaplayer.useAlternateUserAgent"))
+		m_useragent = eConfigManager::getConfigValue("config.mediaplayer.alternateUserAgent");
 }
 
 eServiceMP3Record::~eServiceMP3Record()
