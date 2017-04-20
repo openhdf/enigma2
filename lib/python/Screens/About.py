2400cefcea790788387566ea652f1807d956acaf
Koivo <koivo@live.de>
2017-04-20 18:37:40 +0200

    [formuler] add all models

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -78,11 +78,11 @@ class About(Screen):
 
 		if getMachineBuild() in ('vusolo4k'):
 			cpuMHz = "   (1,5 GHz)"
-		elif getMachineBuild() in ('dm900'):
+		elif getMachineBuild() in ('vuuno4k','dm900', 'gb7252', 'dags7252'):
 			cpuMHz = "   (1,7 GHz)"
-		elif getMachineBuild() in ('formuler1', 'triplex'):
+		elif getMachineBuild() in ('formuler1tc','formuler1', 'triplex'):
 			cpuMHz = "   (1,3 GHz)"
-		elif getMachineBuild() in ('hd52', 'hd51', 'sf4008'):
+		elif getMachineBuild() in ('et1x000','hd52','hd51','sf4008','vs1500','h7'):
 			try:
 				import binascii
 				f = open('/sys/firmware/devicetree/base/cpus/cpu@0/clock-frequency', 'rb')
