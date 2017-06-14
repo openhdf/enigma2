def26519429e317c240e04a35cc3a4c82d09229e
Koivo <koivo@live.de>
2017-06-14 17:11:15 +0200

    [Flash_online] prepare for 6.2

--- lib/python/Plugins/SystemPlugins/SoftwareManager/Flash_online.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/Flash_online.py
@@ -38,7 +38,8 @@ imagesCounter = 0
 #images.append(["V5.5", "http://v55.hdfreaks.cc"])
 #images.append(["V6.0", "http://beta6.hdfreaks.cc"])
 images.append(["V5.5", "http://teamimages.hdfreaks.cc"])
-images.append(["V6.1", "http://v55.hdfreaks.cc"])
+images.append(["V6.2", "http://v55.hdfreaks.cc"])
+images.append(["V6.1", "http://v62.hdfreaks.cc"])
 images.append(["Team", "http://v60.hdfreaks.cc"])
 
 imagePath = '/media/hdd/images'
