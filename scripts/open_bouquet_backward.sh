434a7ff50bb31164c1665e46f49ff76205a3fb5f
Koivo <koivo@live.de>
2017-08-23 07:34:44 +0200

    [scripts] remove user scripts from enigma

--- scripts/open_bouquet_backward.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# script to open last boquet
-wget -qO /tmp/.message.txt 'http://127.0.0.1/web/remotecontrol?command=103' &  > /dev/null 2>&1
-sleep 1
-wget -qO /tmp/.message.txt 'http://127.0.0.1/web/remotecontrol?command=403' &  > /dev/null 2>&1
--- scripts/open_bouquet_forward.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# script to open next boquet
-wget -qO /tmp/.message.txt 'http://127.0.0.1/web/remotecontrol?command=103' &  > /dev/null 2>&1
-sleep 1
-wget -qO /tmp/.message.txt 'http://127.0.0.1/web/remotecontrol?command=402' &  > /dev/null 2>&1
