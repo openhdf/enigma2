2a2b6d7f9711c6f5b03130ab6cf56acd287f82e4
Koivo <koivo@live.de>
2017-08-24 18:08:41 +0200

    [bsod] change background color

--- main/bsod.cpp
+++ main/bsod.cpp
@@ -239,7 +239,7 @@ void bsodFatal(const char *component)
 	gPainter p(my_dc);
 	p.resetOffset();
 	p.resetClip(eRect(ePoint(0, 0), my_dc->size()));
-	p.setBackgroundColor(gRGB(0x008000));
+	p.setBackgroundColor(gRGB(0x1A1A1A));
 	p.setForegroundColor(gRGB(0xFFFFFF));
 
 	int hd =  my_dc->size().width() == 1920;
