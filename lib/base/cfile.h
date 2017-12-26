6c1d6810321c6fdd0372b2b66d215634e3b39a7e
koivo <koivo@live.de>
2017-12-26 07:54:45 +0100

    merge from local git

--- lib/base/cfile.h
+++ lib/base/cfile.h
@@ -12,18 +12,18 @@ struct CFile
 	CFile(const char *filename, const char *mode)
 		: handle(fopen(filename, mode))
 	{
-#ifdef DEBUG
+/*#ifdef DEBUG
 		if (!handle)
 			eDebug("error %s [%m]",filename);
-#endif
+#endif*/
 	}
 	CFile(const std::string &filename, const char *mode)
 		: handle(fopen(filename.c_str(), mode))
 	{
-#ifdef DEBUG
+/*#ifdef DEBUG
 		if (!handle)
 			eDebug("error %s [%m]",filename.c_str());
-#endif
+#endif*/
 	}
 	~CFile()
 	{
@@ -41,6 +41,8 @@ struct CFile
 	static int writeInt(const char *filename, int value);
 	static int writeStr(const char *filename, std::string value);
 	static int write(const char *filename, const char *value);
+	static std::string read(const std::string &filename);
+	static bool contains_word(const std::string &filename, const std::string &word);
 };
 
 #endif
--- lib/base/httpstream.cpp
+++ lib/base/httpstream.cpp
@@ -177,7 +177,7 @@ int eHttpStream::openUrl(const std::string &url, std::string &newurl)
 	result = sscanf(linebuf, "%99s %d %99s", proto, &statuscode, statusmsg);
 	if (result != 3 || (statuscode != 200 && statuscode != 206 && statuscode != 302))
 	{
-		eDebug("%s: wrong http response code: %d", __FUNCTION__, statuscode);
+		eDebug("[eHttpStream] %s: wrong http response code: %d", __func__, statuscode);
 		goto error;
 	}
 
@@ -204,7 +204,7 @@ int eHttpStream::openUrl(const std::string &url, std::string &newurl)
 		if (playlist && !strncasecmp(linebuf, "http://", 7))
 		{
 			newurl = linebuf;
-			eDebug("%s: playlist entry: %s", __FUNCTION__, newurl.c_str());
+			eDebug("[eHttpStream] %s: playlist entry: %s", __func__, newurl.c_str());
 			break;
 		}
 		if (((statuscode == 301) || (statuscode == 302) || (statuscode == 303) || (statuscode == 307) || (statuscode == 308)) &&
@@ -213,7 +213,7 @@ int eHttpStream::openUrl(const std::string &url, std::string &newurl)
 			newurl = &linebuf[10];
 			if (!extra_headers.empty())
 				newurl.append("#").append(extra_headers);
-			eDebug("%s: redirecting to: %s", __FUNCTION__, newurl.c_str());
+			eDebug("[eHttpStream] %s: redirecting to: %s", __func__, newurl.c_str());
 			break;
 		}
 
@@ -230,7 +230,7 @@ int eHttpStream::openUrl(const std::string &url, std::string &newurl)
 	free(linebuf);
 	return 0;
 error:
-	eDebug("%s failed", __FUNCTION__);
+	eDebug("[eHttpStream] %s failed", __func__);
 	free(linebuf);
 	close();
 	return -1;
@@ -245,7 +245,7 @@ int eHttpStream::open(const char *url)
 	 * Spawn a new thread to establish the connection.
 	 */
 	connectionStatus = BUSY;
-	eDebug("eHttpStream::Start thread");
+	eDebug("[eHttpStream] Start thread");
 	run();
 	return 0;
 }
@@ -262,14 +262,14 @@ void eHttpStream::thread()
 		if (openUrl(currenturl, newurl) < 0)
 		{
 			/* connection failed */
-			eDebug("eHttpStream::Thread end NO connection");
+			eDebug("[eHttpStream] Thread end NO connection");
 			connectionStatus = FAILED;
 			return;
 		}
 		if (newurl == "")
 		{
 			/* we have a valid stream connection */
-			eDebug("eHttpStream::Thread end connection");
+			eDebug("[eHttpStream] Thread end connection");
 			connectionStatus = CONNECTED;
 			return;
 		}
@@ -279,7 +279,7 @@ void eHttpStream::thread()
 		newurl = "";
 	}
 	/* too many redirect / playlist levels */
-	eDebug("eHttpStream::Thread end NO connection");
+	eDebug("[eHttpStream] hread end NO connection");
 	connectionStatus = FAILED;
 	return;
 }
