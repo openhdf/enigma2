358d3c7669c75c20a3078b9429861aa0a67022c2
koivo <koivo@live.de>
2018-09-29 17:03:20 +0200

    [about] fix tab

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -156,7 +156,7 @@ class About(Screen):
 						image = "5"
 				f.close()
 				if bootname: bootname = "   (%s)" %bootname 
-				AboutText += _("Selected Image:\t\t%s") % "STARTUP_" + image + bootname + "\n"
+				AboutText += _("Selected Image:\t%s") % "STARTUP_" + image + bootname + "\n"
 		if path.exists('/boot/STARTUP'):
 			f = open('/boot/STARTUP', 'r')
 			f.seek(22)
