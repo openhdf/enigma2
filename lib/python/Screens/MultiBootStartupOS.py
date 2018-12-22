114223b475f38749cba9a825551c64e07261c952
Henry Schrapel <dende@hdfreaks.cc>
2018-12-22 21:18:16 +0100

    Fix missing Import

--- lib/python/Screens/MultiBootStartupOS.py
+++ lib/python/Screens/MultiBootStartupOS.py
@@ -8,7 +8,7 @@ from Components.Label import Label
 from Components.Sources.StaticText import StaticText
 from Components import Harddisk
 from os import path, listdir, system, makedirs
-from os import path, listdir, system
+import re
 
 class MultiBootStartup(ConfigListScreen, Screen):
 
