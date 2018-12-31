836079093a1dc1fa4e5e3be7ca9d9091abadea09
koivo <koivo@live.de>
2018-12-31 18:46:17 +0100

    Revert "pvrparse: check all values from video enumeration"
    This reverts commit 68ca9a38eaa2fdab2c242fcfb539f03441c27f96.

--- lib/dvb/pvrparse.cpp
+++ lib/dvb/pvrparse.cpp
@@ -944,7 +944,7 @@ int eMPEGStreamParserTS::processPacket(const unsigned char *pkt, off_t offset)
 //			eDebug("[eMPEGStreamParserTS] SC %02x %02x %02x %02x, %02x", pkt[0], pkt[1], pkt[2], pkt[3], pkt[4]);
 			unsigned int sc = pkt[3];
 
-			if (m_streamtype == eDVBVideo::UNKNOWN)
+			if (m_streamtype < eDVBVideo::MPEG2) /* unknown */
 			{
 				if ((sc == 0x00) || (sc == 0xb3) || (sc == 0xb8))
 				{
@@ -963,7 +963,7 @@ int eMPEGStreamParserTS::processPacket(const unsigned char *pkt, off_t offset)
 
 			switch(m_streamtype)
 			{
-				case eDVBVideo::MPEG2:
+				case(eDVBVideo::MPEG2): // mpeg2
 				{
 					if ((sc == 0x00) || (sc == 0xb3) || (sc == 0xb8)) /* picture, sequence, group start code */
 					{
@@ -994,7 +994,7 @@ int eMPEGStreamParserTS::processPacket(const unsigned char *pkt, off_t offset)
 					break;
 				}
 
-				case eDVBVideo::MPEG4_H264:
+				case(eDVBVideo::MPEG4_H264): // h.264 */
 				{
 					if (sc == 0x09)
 					{
@@ -1017,7 +1017,7 @@ int eMPEGStreamParserTS::processPacket(const unsigned char *pkt, off_t offset)
 					break;
 				}
 
-				case eDVBVideo::H265_HEVC:
+				case(eDVBVideo::H265_HEVC): // h.265
 				{
 					int nal_unit_type = (sc >> 1);
 					if (nal_unit_type == 35) /* H265 NAL unit access delimiter */
@@ -1037,14 +1037,11 @@ int eMPEGStreamParserTS::processPacket(const unsigned char *pkt, off_t offset)
 					break;
 				}
 
-				case eDVBVideo::UNKNOWN:
-				case eDVBVideo::VC1:
-				case eDVBVideo::MPEG4_Part2:
-				case eDVBVideo::VC1_SM:
-				case eDVBVideo::MPEG1
-				case eDVBVideo::AVS:
+				default:
 				{
-					break; /* TODO: add parser for above codecs */
+					eDebug("[eMPEGStreamParserTS]: unknown streamtype: %d ", m_streamtype);
+
+					break;
 				}
 			}
 		}
