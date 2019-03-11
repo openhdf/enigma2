676e83f3bf461bb9110e54c453fcbbbd2fcfd345
prl <prl@ozemail.com.au>
2019-03-11 16:07:02 +1100

    Allow ConfigList screens to be used from Wizards
    Add a run() method to Components.ConfigList.
    
    This allows screens that inherit from ConfigList (including Setup)
    to be used in wizards.
    
    This is a precursor to allowing time zone setup in the
    startup wizard.

--- lib/python/Components/ConfigList.py
+++ lib/python/Components/ConfigList.py
@@ -310,6 +310,10 @@ class ConfigListScreen:
 		if result:
 			quitMainloop(3)
 
+	# Allow ConfigList screens to be used from Wizards
+	def run(self):
+		self.keySave()
+
 	# keySave and keyCancel are just provided in case you need them.
 	# you have to call them by yourself.
 	def keySave(self):
