b0170c42855ea3bb5e8ea1ab1f057c9777540212
koivo <koivo@live.de>
2019-09-22 10:23:00 +0200

    add osmio4kplus

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -144,7 +144,7 @@ class About(Screen):
 			cpuMHz = "   (1,3 GHz)"
 		elif getMachineBuild() in ('u5','u51','u52','u53','u5pvr','h9','sf8008','sf8008s','sf8008t','hd60',"hd61",'i55plus'):
 			cpuMHz = "   (1,6 GHz)"
-		elif getMachineBuild() in ('sf5008','et13000','et1x000','hd52','hd51','sf4008','vs1500','h7','osmio4k'):
+		elif getMachineBuild() in ('sf5008','et13000','et1x000','hd52','hd51','sf4008','vs1500','h7','osmio4k','osmio4kplus'):
 			try:
 				import binascii
 				f = open('/sys/firmware/devicetree/base/cpus/cpu@0/clock-frequency', 'rb')
