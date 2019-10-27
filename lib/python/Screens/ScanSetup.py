1a58dd06b3421c49be7905dfc4bffec9bebbaa11
koivo <koivo@live.de>
2019-10-27 08:22:46 +0100

    [scansetup] Adapt PositionerSetup to use DVB-S frequencies in KHz merge remote commit from https://github.com/Openeight/enigma2/commits?author=betacentauri

--- lib/python/Screens/ScanSetup.py
+++ lib/python/Screens/ScanSetup.py
@@ -1002,7 +1002,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 		defaultSat = {
 			"orbpos": 192,
 			"system": eDVBFrontendParametersSatellite.System_DVB_S,
-			"frequency": 11836,
+			"frequency": 11836000,
 			"inversion": eDVBFrontendParametersSatellite.Inversion_Unknown,
 			"symbolrate": 27500,
 			"polarization": eDVBFrontendParametersSatellite.Polarisation_Horizontal,
@@ -1043,7 +1043,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 			ttype = frontendData.get("tuner_type", "UNKNOWN")
 			if ttype == "DVB-S":
 				defaultSat["system"] = frontendData.get("system", eDVBFrontendParametersSatellite.System_DVB_S)
-				defaultSat["frequency"] = frontendData.get("frequency", 0) / 1000
+				defaultSat["frequency"] = frontendData.get("frequency", defaultSat["frequency"])
 				defaultSat["inversion"] = frontendData.get("inversion", eDVBFrontendParametersSatellite.Inversion_Unknown)
 				defaultSat["symbolrate"] = frontendData.get("symbol_rate", 0) / 1000
 				defaultSat["polarization"] = frontendData.get("polarization", eDVBFrontendParametersSatellite.Polarisation_Horizontal)
@@ -1131,7 +1131,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 		self.scan_sat.system = ConfigSelection(default = defaultSat["system"], choices = [
 			(eDVBFrontendParametersSatellite.System_DVB_S, _("DVB-S")),
 			(eDVBFrontendParametersSatellite.System_DVB_S2, _("DVB-S2"))])
-		self.scan_sat.frequency = ConfigInteger(default = defaultSat["frequency"], limits = (1, 99999))
+		self.scan_sat.frequency = ConfigFloat(default = [defaultSat["frequency"] / 1000, defaultSat["frequency"] % 1000], limits = [(1, 99999), (0,999)])
 		self.scan_sat.inversion = ConfigSelection(default = defaultSat["inversion"], choices = [
 			(eDVBFrontendParametersSatellite.Inversion_Off, _("Off")),
 			(eDVBFrontendParametersSatellite.Inversion_On, _("On")),
@@ -1444,7 +1444,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 		parm = eDVBFrontendParametersSatellite()
 		parm.modulation = modulation
 		parm.system = system
-		parm.frequency = frequency * 1000
+		parm.frequency = frequency
 		parm.symbol_rate = symbol_rate * 1000
 		parm.polarisation = polarisation
 		parm.fec = fec
@@ -1531,7 +1531,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 							fec = self.scan_sat.fec_s2.value
 						print "add sat transponder"
 
-						self.addSatTransponder(tlist, self.scan_sat.frequency.value,
+						self.addSatTransponder(tlist, self.scan_sat.frequency.floatint,
 									self.scan_sat.symbolrate.value,
 									self.scan_sat.polarization.value,
 									fec,
@@ -1550,7 +1550,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 						tps = nimmanager.getTransponders(orbpos)
 						if len(tps) and len(tps) > self.preDefTransponders.index:
 							tp = tps[self.preDefTransponders.index]
-							self.addSatTransponder(tlist, tp[1] / 1000, tp[2] / 1000, tp[3], tp[4], tp[7], orbpos, tp[5], tp[6], tp[8], tp[9], tp[10], tp[11], tp[12], tp[13], tp[14])
+							self.addSatTransponder(tlist, tp[1], tp[2] / 1000, tp[3], tp[4], tp[7], orbpos, tp[5], tp[6], tp[8], tp[9], tp[10], tp[11], tp[12], tp[13])
 					elif self.scan_type.value == "single_satellite":
 						getInitialTransponderList(tlist, orbpos)
 
@@ -1703,7 +1703,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 
 			compare = [
 				0, # DVB type
-				self.scan_sat.frequency.value*1000,  # 1
+				self.scan_sat.frequency.floatint,    # 1
 				self.scan_sat.symbolrate.value*1000, # 2
 				self.scan_sat.polarization.value,    # 3
 				fec,                                 # 4
@@ -1739,7 +1739,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 		if tp[3] in range (4) and tp[4] in range (11):
 			pol_list = ['H','V','L','R']
 			fec_list = ['Auto','1/2','2/3','3/4','5/6','7/8','8/9','3/5','4/5','9/10','None']
-			tp_text = str(tp[1] / 1000) + " " + pol_list[tp[3]] + " " + str(tp[2] / 1000) + " " + fec_list[tp[4]]
+			tp_text = str(tp[1] / 1000.) + " " + pol_list[tp[3]] + " " + str(tp[2] / 1000) + " " + fec_list[tp[4]]
 			if tp[5] == eDVBFrontendParametersSatellite.System_DVB_S2:
 				if tp[10] > eDVBFrontendParametersSatellite.No_Stream_Id_Filter:
 					tp_text = ("%s MIS %d") % (tp_text, tp[10])
