4bba221a1e4785157b40710c64d675de7b70b740
IanSav <IanSav@users.noreply.github.com>
2020-02-17 23:10:13 +1100

    [Timezones.py] Refactor and optimise the code

--- lib/python/Components/Timezones.py
+++ lib/python/Components/Timezones.py
@@ -1,7 +1,8 @@
 import xml.etree.cElementTree
 
-from os import environ, unlink, symlink
+from os import environ, unlink, symlink, path
 import time
+from Tools.StbHardware import setRTCoffset
 from boxbranding import getMachineBrand
 
 class Timezones:
@@ -40,6 +41,9 @@ class Timezones:
 		except:
 			from enigma import e_tzset
 			e_tzset()
+
+		if path.exists("/proc/stb/fp/rtc_offset"):
+			setRTCoffset()
 
 	def getTimezoneList(self):
 		return [ str(x[0]) for x in self.timezones ]
