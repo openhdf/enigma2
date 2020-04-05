3d3587dff002607d46689fe7c70d6d44b3a93cab
captain <captain.onboard@web.de>
2020-04-05 11:00:55 +0200

    [hd51] add new control led option

--- lib/python/Components/Lcd.py
+++ lib/python/Components/Lcd.py
@@ -288,6 +288,12 @@ class LCD:
 			f.write(configElement.value)
 			f.close()
 
+	def setPowerLEDstate2(configElement):
+		if fileExists("/proc/stb/power/powerled2"):
+			f = open("/proc/stb/power/powerled2", "w")
+			f.write(configElement.value)
+			f.close()
+
 	def setPowerLEDdeepstanbystate(configElement):
 		if fileExists("/proc/stb/power/suspendled"):
 			f = open("/proc/stb/power/suspendled", "w")
@@ -332,6 +338,10 @@ class LCD:
 
 	config.usage.lcd_powerled = ConfigSelection(default = "on", choices = [("off", _("Off")), ("on", _("On"))])
 	config.usage.lcd_powerled.addNotifier(setPowerLEDstate)
+
+	config.usage.lcd_powerled2 = ConfigSelection(default = "on", choices = [("off", _("Off")), ("on", _("On"))])
+	config.usage.lcd_powerled2.addNotifier(setPowerLEDstate2)
+
 	config.usage.lcd_standbypowerled = ConfigSelection(default = "on", choices = [("off", _("Off")), ("on", _("On"))])
 	config.usage.lcd_standbypowerled.addNotifier(setPowerLEDstanbystate)
 
