cd8e43e6f9c1fe550f7cb59f0b8d6708436d97f6
koivo <koivo@live.de>
2020-04-16 19:48:33 +0200

    Revert "[language] update de.po"
    This reverts commit bcbedced55d5cc28de29431eb75e99abd82fc273.

--- lib/python/Plugins/SystemPlugins/SoftwareManager/plugin.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/plugin.py
@@ -385,8 +385,7 @@ class UpdatePluginMenu(Screen):
 						self.session.openWithCallback(self.backupDone,BackupScreen, runBackup = True)
 						self.session.open(FlashOnline)
 					else:
-						#self.session.openWithCallback(self.doBackup, MessageBox, _("Do you want to backup your image and settings before?"), default = True)
-						self.session.open(FlashOnline)
+						self.session.openWithCallback(self.doBackup, MessageBox, _("Do you want to backup your image and settings before?"), default = True)
 				elif (currentEntry == "multiboot-manager"):
 					self.session.open(MultiBootWizard)
 				elif (currentEntry == "backup-image"):
