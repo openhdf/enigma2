f7e952132d286d4982111505cd452c8509db4051
koivo <koivo@live.de>
2020-05-30 07:46:53 +0200

    add some files via upload

--- lib/base/console.cpp
+++ lib/base/console.cpp
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 
 int bidirpipe(int pfd[], const char *cmd , const char * const argv[], const char *cwd )
 {
@@ -38,8 +39,8 @@ int bidirpipe(int pfd[], const char *cmd , const char * const argv[], const char
 		for (unsigned int i=3; i < 90; ++i )
 			close(i);
 
-		if (cwd)
-			chdir(cwd);
+		if (cwd && chdir(cwd) < 0)
+			eDebug("[eConsoleAppContainer] failed to change directory to %s (%m)", cwd);
 
 		execvp(cmd, (char * const *)argv);
 				/* the vfork will actually suspend the parent thread until execvp is called. thus it's ok to use the shared arg/cmdline pointers here. */
@@ -237,7 +238,7 @@ void eConsoleAppContainer::readyRead(int what)
 //		eDebug("[eConsoleAppContainer] readyRead what = %d", what);
 		char* buf = &buffer[0];
 		int rd;
-		while((rd = read(fd[0], buf, buffer.size())) > 0)
+		while((rd = read(fd[0], buf, buffer.size()-1)) > 0)
 		{
 			buf[rd]=0;
 			/*emit*/ dataAvail(std::make_pair(buf, rd));
@@ -276,7 +277,7 @@ void eConsoleAppContainer::readyErrRead(int what)
 //		eDebug("[eConsoleAppContainer] readyErrRead what = %d", what);
 		char* buf = &buffer[0];
 		int rd;
-		while((rd = read(fd[2], buf, buffer.size())) > 0)
+		while((rd = read(fd[2], buf, buffer.size()-1)) > 0)
 		{
 /*			for ( int i = 0; i < rd; i++ )
 				eDebug("[eConsoleAppContainer] %d = %c (%02x)", i, buf[i], buf[i] );*/
