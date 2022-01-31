00203f5b3c05d897056250d4b022ee8eef398bb7
henrylicious <dende@hdfreaks.cc>
2022-01-31 09:04:25 +0100

    use cache

--- lib/python/Tools/LoadPixmap.py
+++ lib/python/Tools/LoadPixmap.py
@@ -1,17 +1,29 @@
 from __future__ import absolute_import
-from enigma import loadPNG, loadJPG
+from enigma import loadPNG, loadJPG, loadSVG
 
 
-def LoadPixmap(path, desktop=None, cached=False):
+# If cached is not supplied, LoadPixmap defaults to caching PNGs and not caching JPGs
+# Split alpha channel JPGs are never cached as the C++ layer's caching is based on
+# a single file per image in the cache
+
+
+def LoadPixmap(path, desktop=None, cached=None, width=0, height=0):
 	if path[-4:] == ".png":
-		ptr = loadPNG(path)
+		# cache unless caller explicity requests to not cache
+		ptr = loadPNG(path, 0, 0 if cached is False else 1)
 	elif path[-4:] == ".jpg":
-		ptr = loadJPG(path)
+		# don't cache unless caller explicity requests caching
+		ptr = loadJPG(path, 1 if cached is True else 0)
+	elif path[-4:] == ".svg":
+		from skin import parameters, getSkinFactor # imported here to avoid circular import
+		autoscale = int(parameters.get("AutoscaleSVG", -1)) # skin_default only == -1, disabled == 0 or enabled == 1
+		scale = height == 0 and (autoscale == -1 and "/skin_default/" in path or autoscale == 1) and getSkinFactor() or 0
+		ptr = loadSVG(path, 0 if cached is False else 1, width, height, scale)
 	elif path[-1:] == ".":
 		alpha = loadPNG(path + "a.png")
 		ptr = loadJPG(path + "rgb.jpg", alpha)
 	else:
-		raise Exception("neither .png nor .jpg, please fix file extension")
+		raise Exception("Neither .png nor .jpg nor .svg, please fix file extension")
 	if ptr and desktop:
 		desktop.makeCompatiblePixmap(ptr)
 	return ptr
