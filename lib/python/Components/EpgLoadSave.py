51cf4a5011a1e9b48e0c92bb1d743d66a096ad4c
koivo <koivo@live.de>
2022-03-06 13:44:42 +0100

    EpgLoadSave fix import

--- lib/python/Components/EpgLoadSave.py
+++ lib/python/Components/EpgLoadSave.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from enigma import eTimer
 
-from Components.Task import Task
+import Components.Task
 from Screens.MessageBox import MessageBox
 from Components.config import config
 
