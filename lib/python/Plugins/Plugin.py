9b0e2670e07ad63f2173c00dcc3acdba484e84fa
koivo <koivo@live.de>
2022-11-05 17:24:53 +0100

    [iservice] add alternativeurl

--- lib/python/Plugins/Plugin.py
+++ lib/python/Plugins/Plugin.py
@@ -67,6 +67,8 @@ class PluginDescriptor:
 
 	WHERE_VIXMENU = 17
 
+	WHERE_CHANNEL_ZAP = 23
+
 	def __init__(self, name="Plugin", where=None, description="", icon=None, fnc=None, wakeupfnc=None, needsRestart=None, internal=False, weight=0):
 		if not where:
 			where = []
