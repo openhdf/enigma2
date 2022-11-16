8ab0bcc4142bf1e6c3311451607478a195b73f2f
koivo <koivo@live.de>
2022-11-16 12:42:10 +0100

    Revert "modify notification"
    This reverts commit a958c7aef5f4fc0b93a1685c3b94a8671065fd6f.

--- lib/python/Tools/Notifications.py
+++ lib/python/Tools/Notifications.py
@@ -1,5 +1,3 @@
-from Screens.MessageBox import MessageBox
-
 notifications = []
 
 notificationAdded = []
@@ -39,11 +37,6 @@ def AddNotificationWithIDCallback(fnc, id, screen, *args, **kwargs):
 # we don't support notifications with callback and ID as this
 # would require manually calling the callback on cancelled popups.
 
-def AddNotificationWithUniqueIDCallback(fnc, id, screen, *args, **kwargs):
-	for x in notifications:
-		if x[4] and x[4] == id:    # Already there...
-			return
-	__AddNotification(fnc, screen, id, *args, **kwargs)
 
 def RemovePopup(id):
 	# remove similiar notifications
@@ -59,6 +52,7 @@ def RemovePopup(id):
 			x[1].close()
 
 
+from Screens.MessageBox import MessageBox
 
 
 def AddPopup(text, type, timeout, id=None):
