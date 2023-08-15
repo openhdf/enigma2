a8a998b21190e230a2fbb0c9822a0455c0e214d8
koivo <koivo@live.de>
2023-08-15 18:02:13 +0200

    [Console] fix bullshit

--- lib/python/Screens/Console.py
+++ lib/python/Screens/Console.py
@@ -1,14 +1,12 @@
-
+from __future__ import print_function
 from enigma import eConsoleAppContainer
-from six import ensure_str
-
+from Screens.Screen import Screen
 from Components.ActionMap import ActionMap
-from Components.Label import Label
 from Components.ScrollLabel import ScrollLabel
 from Components.Sources.StaticText import StaticText
 from Screens.MessageBox import MessageBox
-from Screens.Screen import Screen
-
+from Components.Label import Label
+from six import ensure_str
 
 class Console(Screen):
 	def __init__(self, session, title="Console", cmdlist=None, finishedCallback=None, closeOnSuccess=False):
@@ -17,26 +15,27 @@ class Console(Screen):
 		self.finishedCallback = finishedCallback
 		self.closeOnSuccess = closeOnSuccess
 		self.errorOcurred = False
-		self.hideflag = True
-		self.Shown = True
 
 		self["key_red"] = Label(_("Cancel"))
 		self["key_yellow"] = Label(_("hide"))
 
 		self["text"] = ScrollLabel("")
 		self["summary_description"] = StaticText("")
-		self["actions"] = ActionMap(["ColorActions", "WizardActions", "DirectionActions"],
+		self["actions"] = ActionMap(["WizardActions", "DirectionActions", "ColorActions"],
 		{
 			"ok": self.cancel,
 			"back": self.cancel,
-			"up": self["text"].pageUp,
-			"down": self["text"].pageDown,
-			"yellow": self.yellow,
-			"red": self.key_red
+			"up": self.key_up,
+			"down": self.key_down,
+			"red": self.key_red,
+			"yellow": self.key_yellow
 		}, -1)
 
 		self.cmdlist = cmdlist
 		self.newtitle = title
+
+		self.screen_hide = False
+		self.cancel_msg = None
 
 		self.onShown.append(self.updateTitle)
 
@@ -58,50 +57,71 @@ class Console(Screen):
 	def startRun(self):
 		self["text"].setText(_("Start Execution:") + "\n\n")
 		self["summary_description"].setText(_("Execution:"))
-		print("Console: executing in run", self.run, " the command:", self.cmdlist[self.run])
+		print("[Console] executing in run", self.run, " the command:", self.cmdlist[self.run])
 		if self.doExec(self.cmdlist[self.run]): #start of container application failed...
 			self.runFinished(-1) # so we must call runFinished manual
 
 	def runFinished(self, retval):
 		if retval:
 			self.errorOcurred = True
-			self.hideScreen = True
+			self.toggleScreenHide(True)
 		self.run += 1
 		if self.run != len(self.cmdlist):
 			if self.doExec(self.cmdlist[self.run]): #start of container application failed...
 				self.runFinished(-1) # so we must call runFinished manual
 		else:
+			self["key_red"].setText(_("Close"))
+			self["key_yellow"].setText(_(" "))
+			self.toggleScreenHide(True)
+			if self.cancel_msg:
+				self.cancel_msg.close()
 			lastpage = self["text"].isAtLastPage()
-			str = self["text"].getText()
-			str += _("\nExecution finished!!")
+			self["text"].appendText(_("\nExecution finished!!"))
 			self["summary_description"].setText(_("\nExecution finished!!"))
-			self["text"].setText(str)
-			if lastpage:
-				self["text"].lastPage()
 			if self.finishedCallback is not None:
 				self.finishedCallback()
 			if not self.errorOcurred and self.closeOnSuccess:
 				self.cancel()
 
-	def hideScreen(self, setshow=False):
-		if self.hideflag or setshow:
+	def key_up(self):
+		if self.screen_hide:
+			self.toggleScreenHide()
+			return
+		self["text"].pageUp()
+
+	def key_down(self):
+		if self.screen_hide:
+			self.toggleScreenHide()
+			return
+		self["text"].pageDown()
+
+	def key_red(self):
+		if self.screen_hide:
+			self.toggleScreenHide()
+			return
+		if self.run == len(self.cmdlist):
+			self.cancel(True)
+		else:
+			self.cancel_msg = self.session.openWithCallback(self.cancelCB, MessageBox, _("Really abort execution?"), type=MessageBox.TYPE_YESNO, default=False)
+
+	def key_yellow(self):
+		if self.screen_hide:
+			self.toggleScreenHide()
+			return
+		else:
+			if self.run != len(self.cmdlist):
+				self.toggleScreenHide()
+
+	def toggleScreenHide(self, setshow=False):
+		if self.screen_hide or setshow:
 			self.show()
 		else:
 			self.hide()
-		self.hideflag = not (self.hideflag or setshow)
-
-	def yellow(self):
-		print('Yellow Pressed')
-		if self.Shown == True:
-			self.hideScreen()
-			self.Shown = False
-		else:
-			self.show()
-			self.Shown = True
+		self.screen_hide = not (self.screen_hide or setshow)
 
 	def cancel(self, force=False):
-		if self.hideflag:
-			self.hideScreen()
+		if self.screen_hide:
+			self.toggleScreenHide()
 			return
 		if force or self.run == len(self.cmdlist):
 			self.close()
@@ -109,15 +129,6 @@ class Console(Screen):
 			self.container.dataAvail.remove(self.dataAvail)
 			if self.run != len(self.cmdlist):
 				self.container.kill()
-
-	def key_red(self):
-		if self.hideflag:
-			self.hideScreen()
-			return
-		if self.run == len(self.cmdlist):
-			self.cancel()
-		else:
-			self.cancel_msg = self.session.openWithCallback(self.cancelCB, MessageBox, _("Really abort execution?"), type=MessageBox.TYPE_YESNO, default=False)
 
 	def cancelCB(self, ret=None):
 		self.cancel_msg = None
