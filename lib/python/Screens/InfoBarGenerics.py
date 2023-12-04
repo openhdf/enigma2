94a1c1e4c156ddd288e39e63748fb1e46de33457
Littlesat <littlesat@openpli.org>
2023-12-04 16:05:12 +0100

    Further cleanup some stream relay related code
    Also thanks to Huevos for the notificiation.
    Now also when stop using streamrelay also restart the service when it is
    the current service so you also see the result immediataly when you
    unselect a current running service.
    
    (cherry picked from commit 66e91a52a9b6e6dfd44d3cabf7ae27e5194c3eaa)

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -1108,15 +1108,15 @@ class InfoBarShowHide(InfoBarScreenSaver):
 				self.eventView = self.session.openWithCallback(self.closed, EventViewSimple, epglist[0], ServiceReference(ref))
 				self.dlg_stack = None
 
-	def ToggleStreamrelay(self, service=None):
-		service = service or self.session.nav.getCurrentlyPlayingServiceReference()
-		if service:
-			service = service.toString()
-			if service in whitelist.streamrelay:
-				whitelist.streamrelay.remove(service)
-			else:
-				whitelist.streamrelay.append(service)
-			open('/etc/enigma2/whitelist_streamrelay', 'w').write('\n'.join(whitelist.streamrelay))
+	def ToggleStreamrelay(self, service):
+		servicestring = service.toString()
+		if servicestring in whitelist.streamrelay:
+			whitelist.streamrelay.remove(servicestring)
+		else:
+			whitelist.streamrelay.append(servicestring)
+		if self.session.nav.getCurrentlyPlayingServiceReference() == service:
+			self.session.nav.restartService()
+		open('/etc/enigma2/whitelist_streamrelay', 'w').write('\n'.join(whitelist.streamrelay))
 
 	def getNowNext(self):
 		epglist = []
