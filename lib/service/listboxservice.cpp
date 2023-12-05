39a19d8a91814b83e04bb2b98241a703d3243912
Huevos <rowland@moralet.com>
2023-12-05 22:04:02 +0100

    [listboxservice] compareServices, avoid some false positives
    This problem occured when IPTV services were not using unique service references
    
    (cherry picked from commit dc4c20366857f2cc27111f5d1b2afa7c87b297ae)

--- lib/service/listboxservice.cpp
+++ lib/service/listboxservice.cpp
@@ -12,6 +12,59 @@
 
 ePyObject eListboxServiceContent::m_GetPiconNameFunc;
 
+std::string toLower(std::string& data) {
+	std::string data_tmp = data;
+	std::transform(data_tmp.begin(), data_tmp.end(), data_tmp.begin(), tolower);
+    return data_tmp;
+}
+
+// Next two functions are used for finding correct recording in case of dynamic iptv service url
+void join(const std::vector<std::string>& v, char c, std::string& s) {
+
+   s.clear();
+
+   for (std::vector<std::string>::const_iterator p = v.begin();
+        p != v.end(); ++p) {
+      s += *p;
+      if (p != v.end() - 1)
+        s += c;
+   }
+}
+
+bool compareServices(const eServiceReference &ref1, const eServiceReference &ref2, bool alternativeMatching) {
+	std::vector<std::string> ref_split = split(ref1.toString(), ":");
+	std::vector<std::string> s_split = split(ref2.toString(), ":");
+
+	if (ref_split[1] == "7" || s_split[1] == "7") {
+		return ref1 == ref2;
+	}
+
+	std::vector<std::string> ref_split_r(ref_split.begin(), ref_split.begin() + 10);
+	std::string ref_s;
+	join(ref_split_r, ':', ref_s);
+
+	std::vector<std::string> s_split_r(s_split.begin(), s_split.begin() + 10);
+	std::string s_s;
+	join(s_split_r, ':', s_s);
+
+	if (!alternativeMatching) {
+		if (ref1 == ref2) return true;
+	} else {
+		if (ref_s == s_s) return true;
+	}
+	// If "127.0.0.1" is in the service reference this is probably a stream relay
+	// so use partial matching logic
+	if (ref2.toString().find("127.0.0.1") != std::string::npos) {
+		std::string url_sr = s_split[s_split.size() - 2];
+		std::vector<std::string> sr_split = split(url_sr, "/");
+		std::string ref_orig = sr_split.back();
+		ref_orig = replace_all(ref_orig, "%3a", ":");
+		return r_i.toString() == ref_orig;
+	}
+
+	return false;
+}
+
 void eListboxServiceContent::addService(const eServiceReference &service, bool beforeCurrent)
 {
 	if (beforeCurrent && m_size)
@@ -608,14 +661,19 @@ bool eListboxServiceContent::checkServiceIsRecorded(eServiceReference ref,pNavig
 			ePtr<eDVBResourceManager> res;
 			eDVBResourceManager::getInstance(res);
 			res->getChannelList(db);
-			eBouquet *bouquet=0;
-			db->getBouquet(ref, bouquet);
-			for (std::list<eServiceReference>::iterator i(bouquet->m_services.begin()); i != bouquet->m_services.end(); ++i)
-				if (*i == it->second)
-					return true;
+			eBouquet *bouquet = NULL;
+			if (!db->getBouquet(ref, bouquet))
+			{
+				for (std::list<eServiceReference>::iterator i(bouquet->m_services.begin()); i != bouquet->m_services.end(); ++i){
+					if (compareServices(*i, it->second, m_alternative_record_match))
+						return true;
+				}
+			}
 		}
-		else if (ref == it->second)
-			return true;
+		else {
+			if (compareServices(ref, it->second, m_alternative_record_match))
+				return true;
+		}
 	}
 	return false;
 }
