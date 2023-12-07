c81d30fc0becfed98f76022633c4774bc17f4fb8
henrylicious <dende@hdfreaks.cc>
2023-12-07 13:28:12 +0100

    fix some merging erros with streamrelay

--- lib/python/Navigation.py
+++ lib/python/Navigation.py
@@ -324,7 +324,7 @@ class Navigation:
 				playref = ref
 			if self.pnav:
 				self.currentlyPlayingServiceReference = playref
-				playref = self.streamrelayChecker(playref)
+				playref = streamrelayChecker(playref)
 				self.currentlyPlayingServiceOrGroup = ref
 				if InfoBarInstance and InfoBarInstance.servicelist.servicelist.setCurrent(ref, adjust):
 					self.currentlyPlayingServiceOrGroup = InfoBarInstance.servicelist.servicelist.getCurrent()
@@ -354,14 +354,6 @@ class Navigation:
 			self.currentlyPlayingServiceOrGroup = InfoBarInstance.servicelist.servicelist.getCurrent()
 		return 1
 
-	def streamrelayChecker(self, playref):
-		playrefstring = playref.toString()
-		if '%3a//' not in playrefstring and playrefstring in whitelist.streamrelay:
-			url = "http://%s:%s/" % (config.misc.softcam_streamrelay_url.getHTML(), config.misc.softcam_streamrelay_port.value)
-			playref = eServiceReference("%s%s%s:%s" % (playrefstring, url.replace(":", "%3a"), playrefstring.replace(":", "%3a"), ServiceReference(playref).getServiceName()))
-			print("[Navigation] Play or record service via streamrelay as it is whitelisted as such", playref.toString())
-		return playref
-
 	def getCurrentlyPlayingServiceReference(self):
 		return self.currentlyPlayingServiceReference
 
@@ -384,7 +376,7 @@ class Navigation:
 		if ref:
 			if ref.flags & eServiceReference.isGroup:
 				ref = getBestPlayableServiceReference(ref, eServiceReference(), simulate)
-			ref = self.streamrelayChecker(ref)
+			ref = streamrelayChecker(ref)
 			service = ref and self.pnav and self.pnav.recordService(ref, simulate, type)
 			if service is None:
 				print("record returned non-zero")
