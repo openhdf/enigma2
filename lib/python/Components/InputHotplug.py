f62faccac422693af750d4a500479f2dde584210
koivo <koivo@live.de>
2025-06-26 18:17:24 +0200

    [InputHotplug] fix wrong import

--- lib/python/Components/InputHotplug.py
+++ lib/python/Components/InputHotplug.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
-
-from os import path as os_path
-
-from enigma import addInputDevice, removeInputDevice
-
-from Components import Netlink
+import Components.Netlink
+import enigma
+import os
 
 
-class NetlinkReader():
+class NetlinkReader:
 	def __init__(self):
 		from twisted.internet import reactor
-		self.nls = Netlink.NetlinkSocket()
+		self.nls = Components.Netlink.NetlinkSocket()
 		reactor.addReader(self)
 
 	def fileno(self):
@@ -25,10 +22,10 @@ class NetlinkReader():
 					action = event['ACTION']
 					if action == 'add':
 						print("New input device detected:", devname)
-						addInputDevice(os_path.join('/dev', devname))
+						enigma.addInputDevice(os.path.join('/dev', devname))
 					elif action == 'remove':
 						print("Removed input device:", devname)
-						removeInputDevice(os_path.join('/dev', devname))
+						enigma.removeInputDevice(os.path.join('/dev', devname))
 				elif subsystem == 'net':
 					from Components.Network import iNetwork
 					iNetwork.hotplug(event)
