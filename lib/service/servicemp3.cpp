e891728bc6d53209a676a05188f68220697b0d47
henrylicious <dende@hdfreaks.cc>
2026-03-30 13:43:23 +0200

    Fix warning: enumeration value 'GST_STATE_....' not handled in switch

--- lib/service/servicemp3.cpp
+++ lib/service/servicemp3.cpp
@@ -2188,11 +2188,12 @@ void eServiceMP3::gstBusCall(GstMessage *msg)
 					m_paused = true;
 				}	break;
 				case GST_STATE_CHANGE_PAUSED_TO_READY:
-				{
-				}	break;
 				case GST_STATE_CHANGE_READY_TO_NULL:
-				{
-				}	break;
+				case GST_STATE_CHANGE_NULL_TO_NULL:
+				case GST_STATE_CHANGE_READY_TO_READY:
+				case GST_STATE_CHANGE_PAUSED_TO_PAUSED:
+				case GST_STATE_CHANGE_PLAYING_TO_PLAYING:
+					break;
 			}
 			break;
 		}
